97ac53f54e15cc90f8a602e30bee5b411bbd788e
Antoine Toulme <antoine@lunar-ocean.com>
2019-06-12 09:16:32 -0700

    Rebase off sketch branch

--- src/test/kotlin/io/libp2p/core/types/UvarintTest.kt
+++ src/test/kotlin/io/libp2p/core/types/UvarintTest.kt
@@ -2,18 +2,31 @@ package io.libp2p.core.types
 
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.lang.IllegalStateException
 import java.nio.charset.StandardCharsets
 
 class UvarintTest {
+
     @Test
     fun testEncodeDecode() {
         val buf = Unpooled.buffer()
         buf.writeUvarint(42L)
         assertEquals(42L, buf.readUvarint())
+    }
+
+    @Test
+    fun testIterateIntegers() {
+        for (i in 0 .. 10000L) {
+            val buf = Unpooled.buffer()
+            buf.writeUvarint(i);
+            assertEquals(i, buf.readUvarint())
+            assertFalse(buf.isReadable)
+        }
     }
 
     @Test
