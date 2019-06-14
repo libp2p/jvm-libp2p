871f731c7a408ec374554a6d6f8c8e9f0a97b3f1
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-06-14 13:28:23 +0300

    Import crypto classes from Web3J impl

---
+++ src/main/kotlin/io/libp2p/core/crypto/Key.kt
@@ -0,0 +1,189 @@
+/*
+ * Copyright 2019 BLK Technologies Limited (web3labs.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.libp2p.core.crypto
+
+import com.google.protobuf.ByteString
+import crypto.pb.Crypto
+import io.libp2p.core.crypto.keys.generateEcdsaKeyPair
+import io.libp2p.core.crypto.keys.generateEd25519KeyPair
+import io.libp2p.core.crypto.keys.generateRsaKeyPair
+import io.libp2p.core.crypto.keys.generateSecp256k1KeyPair
+import io.libp2p.core.crypto.keys.unmarshalEcdsaPrivateKey
+import io.libp2p.core.crypto.keys.unmarshalEcdsaPublicKey
+import io.libp2p.core.crypto.keys.unmarshalEd25519PrivateKey
+import io.libp2p.core.crypto.keys.unmarshalEd25519PublicKey
+import io.libp2p.core.crypto.keys.unmarshalRsaPrivateKey
+import io.libp2p.core.crypto.keys.unmarshalRsaPublicKey
+import io.libp2p.core.crypto.keys.unmarshalSecp256k1PrivateKey
+import io.libp2p.core.crypto.keys.unmarshalSecp256k1PublicKey
+import crypto.pb.Crypto.PrivateKey as PbPrivateKey
+import crypto.pb.Crypto.PublicKey as PbPublicKey
+
+enum class KEY_TYPE {
+
+    /**
+     * RSA is an enum for the supported RSA key type
+     */
+    RSA,
+
+    /**
+     * Ed25519 is an enum for the supported Ed25519 key type
+     */
+    ED25519,
+
+    /**
+     * Secp256k1 is an enum for the supported Secp256k1 key type
+     */
+    SECP256K1,
+
+    /**
+     * ECDSA is an enum for the supported ECDSA key type
+     */
+    ECDSA
+}
+
+interface Key {
+
+    val keyType: crypto.pb.Crypto.KeyType
+
+    /**
+     * Bytes returns a serialized, storeable representation of this key.
+     */
+    @Deprecated("Use marshal/unmarshal functions instead", level = DeprecationLevel.WARNING)
+    fun bytes(): ByteArray
+
+    fun raw(): ByteArray
+}
+
+/**
+ * PrivKey represents a private key that can be used to generate a public key,
+ * sign data, and decrypt data that was encrypted with a public key.
+ */
+abstract class PrivKey(override val keyType: Crypto.KeyType) : Key {
+
+    /**
+     * Cryptographically sign the given bytes.
+     */
+    abstract fun sign(data: ByteArray): ByteArray
+
+    /**
+     * Return a public key paired with this private key.
+     */
+    abstract fun publicKey(): PubKey
+
+    override fun bytes(): ByteArray = marshalPrivateKey(this)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return bytes().contentEquals((other as PrivKey).bytes())
+    }
+}
+
+/**
+ * PubKey is a public key.
+ */
+abstract class PubKey(override val keyType: Crypto.KeyType) : Key {
+
+    /**
+     * Verify that 'sig' is the signed hash of 'data'.
+     */
+    abstract fun verify(data: ByteArray, signature: ByteArray): Boolean
+
+    override fun bytes(): ByteArray = marshalPublicKey(this)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        return bytes().contentEquals((other as PubKey).bytes())
+    }
+}
+
+class BadKeyTypeException : Exception("Invalid or unsupported key type")
+
+/**
+ * Generates a new key pair of the provided type.
+ * @param type the type key to be generated.
+ * @param bits the number of bits desired for the key (only applicable for RSA).
+ */
+fun generateKeyPair(type: KEY_TYPE, bits: Int): Pair<PrivKey, PubKey> {
+
+    return when (type) {
+        KEY_TYPE.RSA -> generateRsaKeyPair(bits)
+        KEY_TYPE.ED25519 -> generateEd25519KeyPair()
+        KEY_TYPE.SECP256K1 -> generateSecp256k1KeyPair()
+        KEY_TYPE.ECDSA -> generateEcdsaKeyPair()
+    }
+}
+
+/**
+ * Converts the protobuf serialized public key into its representative object.
+ * @param data the byte array of the protobuf public key.
+ * @return the equivalent public key.
+ */
+fun unmarshalPublicKey(data: ByteArray): PubKey {
+    val pmes = PbPublicKey.parseFrom(data)
+
+    val pmesd = pmes.data.toByteArray()
+
+    return when (pmes.type) {
+        Crypto.KeyType.RSA -> unmarshalRsaPublicKey(pmesd)
+        Crypto.KeyType.Ed25519 -> unmarshalEd25519PublicKey(pmesd)
+        Crypto.KeyType.Secp256k1 -> unmarshalSecp256k1PublicKey(pmesd)
+        Crypto.KeyType.ECDSA -> unmarshalEcdsaPublicKey(pmesd)
+        else -> throw BadKeyTypeException()
+    }
+}
+
+/**
+ * Converts a public key object into a protobuf serialized public key.
+ * @param pubKey the public key instance.
+ * @return the protobuf bytes.
+ */
+fun marshalPublicKey(pubKey: PubKey): ByteArray =
+    PbPublicKey.newBuilder()
+        .setType(pubKey.keyType)
+        .setData(ByteString.copyFrom(pubKey.raw()))
+        .build()
+        .toByteArray()
+
+/**
+ * Converts a protobuf serialized private key into its representative object.
+ * @param data the byte array of hte protobuf private key.
+ * @return the equivalent private key.
+ */
+fun unmarshalPrivateKey(data: ByteArray): PrivKey {
+    val pmes = PbPrivateKey.parseFrom(data)
+
+    val pmesd = pmes.data.toByteArray()
+
+    return when (pmes.type) {
+        Crypto.KeyType.RSA -> unmarshalRsaPrivateKey(pmesd)
+        Crypto.KeyType.Ed25519 -> unmarshalEd25519PrivateKey(pmesd)
+        Crypto.KeyType.Secp256k1 -> unmarshalSecp256k1PrivateKey(pmesd)
+        Crypto.KeyType.ECDSA -> unmarshalEcdsaPrivateKey(pmesd)
+        else -> throw BadKeyTypeException()
+    }
+}
+
+/**
+ * Converts a public key object into a protobuf serialized private key.
+ * @param privKey the private key.
+ * @return the protobuf bytes.
+ */
+fun marshalPrivateKey(privKey: PrivKey): ByteArray =
+    PbPrivateKey.newBuilder()
+        .setType(privKey.keyType)
+        .setData(ByteString.copyFrom(privKey.raw()))
+        .build()
+        .toByteArray()
---
+++ src/main/kotlin/io/libp2p/core/crypto/keys/Secp256k1.kt
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2019 BLK Technologies Limited (web3labs.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package io.libp2p.core.crypto.keys
+
+import crypto.pb.Crypto
+import io.libp2p.core.Libp2pException
+import io.libp2p.core.crypto.PrivKey
+import io.libp2p.core.crypto.PubKey
+import io.libp2p.core.crypto.SECP_256K1_ALGORITHM
+import org.bouncycastle.asn1.ASN1InputStream
+import org.bouncycastle.asn1.ASN1Integer
+import org.bouncycastle.asn1.ASN1Primitive
+import org.bouncycastle.asn1.ASN1Sequence
+import org.bouncycastle.asn1.DERSequenceGenerator
+import org.bouncycastle.asn1.sec.SECNamedCurves
+import org.bouncycastle.crypto.ec.CustomNamedCurves
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator
+import org.bouncycastle.crypto.params.ECDomainParameters
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters
+import org.bouncycastle.crypto.params.ECPublicKeyParameters
+import org.bouncycastle.crypto.params.ParametersWithRandom
+import org.bouncycastle.crypto.signers.ECDSASigner
+import org.bouncycastle.math.ec.FixedPointCombMultiplier
+import org.bouncycastle.math.ec.FixedPointUtil
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.math.BigInteger
+import java.security.SecureRandom
+
+// The parameters of the secp256k1 curve that Bitcoin uses.
+private val CURVE_PARAMS = CustomNamedCurves.getByName(SECP_256K1_ALGORITHM)
+
+private val CURVE: ECDomainParameters = CURVE_PARAMS.let {
+    FixedPointUtil.precompute(CURVE_PARAMS.g)
+    ECDomainParameters(CURVE_PARAMS.curve, CURVE_PARAMS.g, CURVE_PARAMS.n, CURVE_PARAMS.h)
+}
+
+/**
+ * @param privateKey the private key backing this instance.
+ */
+class Secp256k1PrivateKey(private val privateKey: ECPrivateKeyParameters) : PrivKey(Crypto.KeyType.Secp256k1) {
+
+    private val priv = privateKey.d
+
+    override fun raw(): ByteArray = priv.toByteArray()
+
+    override fun sign(data: ByteArray): ByteArray {
+        val (r, s) = with(ECDSASigner()) {
+            init(true, ParametersWithRandom(privateKey, SecureRandom()))
+            generateSignature(data).let {
+                Pair(it[0], it[1])
+            }
+        }
+
+        return with(ByteArrayOutputStream()) {
+            DERSequenceGenerator(this).run {
+                addObject(ASN1Integer(r))
+                addObject(ASN1Integer(s))
+                close()
+                toByteArray()
+            }
+        }
+    }
+
+    override fun publicKey(): PubKey {
+        val privKey = if (priv.bitLength() > CURVE.n.bitLength()) priv.mod(CURVE.n) else priv
+        val publicPoint = FixedPointCombMultiplier().multiply(CURVE.g, privKey)
+        return Secp256k1PublicKey(ECPublicKeyParameters(publicPoint, CURVE))
+    }
+
+    override fun hashCode(): Int = priv.hashCode()
+}
+
+/**
+ * @param pub the public key backing this instance.
+ */
+class Secp256k1PublicKey(private val pub: ECPublicKeyParameters) : PubKey(Crypto.KeyType.Secp256k1) {
+
+    override fun raw(): ByteArray = pub.q.getEncoded(true)
+
+    override fun verify(data: ByteArray, signature: ByteArray): Boolean {
+        val signer = ECDSASigner().also {
+            it.init(false, pub)
+        }
+
+        val asn1: ASN1Primitive =
+            ByteArrayInputStream(signature)
+                .use { inStream ->
+                    ASN1InputStream(inStream)
+                        .use { asnInputStream ->
+                            asnInputStream.readObject()
+                        }
+                }
+
+        val asn1Encodables = (asn1 as ASN1Sequence).toArray().also {
+            if (it.size != 2) {
+                throw Libp2pException("Invalid signature: expected 2 values for 'r' and 's' but got ${it.size}")
+            }
+        }
+
+        val r = (asn1Encodables[0].toASN1Primitive() as ASN1Integer).value
+        val s = (asn1Encodables[1].toASN1Primitive() as ASN1Integer).value
+        return signer.verifySignature(data, r.abs(), s.abs())
+    }
+
+    override fun hashCode(): Int = pub.hashCode()
+}
+
+/**
+ * Generates a new SECP256K1 private and public key.
+ * @return a pair of the private and public keys.
+ */
+fun generateSecp256k1KeyPair(): Pair<PrivKey, PubKey> = with(ECKeyPairGenerator()) {
+    val domain = SECNamedCurves.getByName(SECP_256K1_ALGORITHM).let {
+        ECDomainParameters(it.curve, it.g, it.n, it.h)
+    }
+    init(ECKeyGenerationParameters(domain, SecureRandom()))
+    val keypair = generateKeyPair()
+
+    val privateKey = keypair.private as ECPrivateKeyParameters
+    return Pair(Secp256k1PrivateKey(privateKey), Secp256k1PublicKey(keypair.public as ECPublicKeyParameters))
+}
+
+/**
+ * Unmarshals the given key bytes into a SECP256K1 private key instance.
+ * @param keyBytes the key bytes.
+ * @return a private key instance.
+ */
+fun unmarshalSecp256k1PrivateKey(data: ByteArray): PrivKey =
+    Secp256k1PrivateKey(ECPrivateKeyParameters(BigInteger(1, data), CURVE))
+
+/**
+ * Unmarshals the given key bytes into a SECP256K1 public key instance.
+ * @param keyBytes the key bytes.
+ * @return a public key instance.
+ */
+fun unmarshalSecp256k1PublicKey(data: ByteArray): PubKey =
+    Secp256k1PublicKey(ECPublicKeyParameters(CURVE.curve.decodePoint(data), CURVE))
