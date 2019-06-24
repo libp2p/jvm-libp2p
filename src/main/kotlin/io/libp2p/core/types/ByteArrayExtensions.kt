b73a53b2caf6035f8de75701256c4ed27fea16f4
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-06-24 19:36:37 +0300

    Fix ByteArray.compareTo

--- src/main/kotlin/io/libp2p/core/types/ByteArrayExtensions.kt
+++ src/main/kotlin/io/libp2p/core/types/ByteArrayExtensions.kt
@@ -11,7 +11,7 @@ fun String.fromHex() =
 operator fun ByteArray.compareTo(other: ByteArray): Int {
     if (size != other.size) return size - other.size
     for (i in 0..size) {
-        if (this[i] != other[i]) return this[i] - other[i]
+        if (this[i] != other[i]) return this[i].toInt().and(0xFF) - other[i].toInt().and(0xFF)
     }
     return 0;
 }
