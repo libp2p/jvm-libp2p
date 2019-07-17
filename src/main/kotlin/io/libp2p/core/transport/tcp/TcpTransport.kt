d3d1ee47abeea95acc2dcf6b2e963fa537ac989a
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-07-17 11:30:08 +0300

    Add a bit more logic to TcpTransport

--- src/main/kotlin/io/libp2p/core/transport/tcp/TcpTransport.kt
+++ src/main/kotlin/io/libp2p/core/transport/tcp/TcpTransport.kt
@@ -1,13 +1,18 @@
 package io.libp2p.core.transport.tcp
 
 import io.libp2p.core.Connection
+import io.libp2p.core.Libp2pException
 import io.libp2p.core.multiformats.Multiaddr
-import io.libp2p.core.multiformats.Protocol
+import io.libp2p.core.multiformats.Protocol.DNSADDR
+import io.libp2p.core.multiformats.Protocol.IP4
+import io.libp2p.core.multiformats.Protocol.IP6
+import io.libp2p.core.multiformats.Protocol.TCP
 import io.libp2p.core.transport.ConnectionUpgrader
 import io.libp2p.core.transport.Transport
 import io.libp2p.core.types.toCompletableFuture
 import io.netty.bootstrap.Bootstrap
 import io.netty.bootstrap.ServerBootstrap
+import java.net.InetSocketAddress
 import java.util.concurrent.CompletableFuture
 
 /**
@@ -28,7 +33,7 @@ class TcpTransport(val upgrader: ConnectionUpgrader) : Transport {
     // Checks if this transport can handle this multiaddr. It should return true for multiaddrs containing `tcp` atoms.
     override fun handles(addr: Multiaddr): Boolean {
         return addr.components
-            .any { pair -> pair.first == Protocol.TCP }
+            .any { pair -> pair.first == TCP }
     }
 
     // Closes this transport entirely, aborting all ongoing connections and shutting down any listeners.
@@ -45,7 +50,16 @@ class TcpTransport(val upgrader: ConnectionUpgrader) : Transport {
     }
 
     override fun dial(addr: Multiaddr): CompletableFuture<Connection> =
-        client.connect().toCompletableFuture()
+        client.connect(fromMultiaddr(addr)).toCompletableFuture()
+            .thenCompose { upgrader.establishSecureChannel(it).toCompletableFuture() }
             .thenCompose { upgrader.establishMuxer(it).toCompletableFuture() }
             .thenApply { Connection(it) }
+
+    private fun fromMultiaddr(addr: Multiaddr): InetSocketAddress {
+        val host = addr.getStringComponents().find { p -> p.first in arrayOf(IP4, IP6, DNSADDR) }
+            ?.second ?: throw Libp2pException("Missing IP4/IP6/DNSADDR in multiaddress $addr")
+        val port = addr.getStringComponents().find { p -> p.first == TCP }
+            ?.second ?: throw Libp2pException("Missing TCP in multiaddress $addr")
+        return InetSocketAddress.createUnresolved(host, port.toInt())
+    }
 }
