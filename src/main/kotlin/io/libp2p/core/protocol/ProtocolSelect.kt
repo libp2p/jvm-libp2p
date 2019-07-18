cf308748858419fc7849f883f9adfd379cd6801e
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-07-18 11:29:00 +0300

    Add Multistream wrapper class

--- src/main/kotlin/io/libp2p/core/protocol/ProtocolSelect.kt
+++ src/main/kotlin/io/libp2p/core/protocol/ProtocolSelect.kt
@@ -3,21 +3,22 @@ package io.libp2p.core.protocol
 import io.libp2p.core.Libp2pException
 import io.libp2p.core.events.ProtocolNegotiationFailed
 import io.libp2p.core.events.ProtocolNegotiationSucceeded
-import io.libp2p.core.security.SecureChannel
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
 
 /**
  * Created by Anton Nashatyrev on 20.06.2019.
  */
-class ProtocolSelect(val protocols: List<SecureChannel> = mutableListOf()) : ChannelInboundHandlerAdapter() {
+class ProtocolSelect<TController>(val protocols: List<ProtocolBinding<TController>> = mutableListOf()) :
+    ChannelInboundHandlerAdapter() {
 
     override fun userEventTriggered(ctx: ChannelHandlerContext, evt: Any) {
         when (evt) {
             is ProtocolNegotiationSucceeded -> {
-                val channel = protocols.find { it.matcher.matches(evt.proto) }
+                val protocolBinding = protocols.find { it.matcher.matches(evt.proto) }
                     ?: throw Libp2pException("Protocol negotiation failed: not supported protocol ${evt.proto}")
-                ctx.pipeline().replace(this, "SecureChannelInitializer", channel.initializer().channelInitializer)
+                ctx.pipeline().replace(this, "ProtocolBindingInitializer"
+                    , protocolBinding.initializer(evt.proto).channelInitializer)
             }
             is ProtocolNegotiationFailed -> throw Libp2pException("ProtocolNegotiationFailed: $evt")
         }
