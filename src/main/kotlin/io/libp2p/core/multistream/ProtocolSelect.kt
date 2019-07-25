e10eda085664ec72762b091b29687ee024319d46
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-07-25 11:35:18 +0300

    Add back the file deleted while merge

---
+++ src/main/kotlin/io/libp2p/core/multistream/ProtocolSelect.kt
@@ -0,0 +1,41 @@
+package io.libp2p.core.multistream
+
+import io.libp2p.core.ConnectionClosedException
+import io.libp2p.core.Libp2pException
+import io.libp2p.core.events.ProtocolNegotiationFailed
+import io.libp2p.core.events.ProtocolNegotiationSucceeded
+import io.libp2p.core.types.forward
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.ChannelInboundHandlerAdapter
+import java.util.concurrent.CompletableFuture
+
+/**
+ * Created by Anton Nashatyrev on 20.06.2019.
+ */
+class ProtocolSelect<TController>(val protocols: List<ProtocolBinding<TController>> = mutableListOf()) :
+    ChannelInboundHandlerAdapter() {
+
+    val selectedFuture = CompletableFuture<TController>()
+
+    override fun userEventTriggered(ctx: ChannelHandlerContext, evt: Any) {
+        when (evt) {
+            is ProtocolNegotiationSucceeded -> {
+                val protocolBinding = protocols.find { it.matcher.matches(evt.proto) }
+                    ?: throw Libp2pException("Protocol negotiation failed: not supported protocol ${evt.proto}")
+                val bindingInitializer = protocolBinding.initializer(evt.proto)
+                bindingInitializer.controller.forward(selectedFuture)
+                ctx.pipeline().replace(this, "ProtocolBindingInitializer", bindingInitializer.channelInitializer)
+            }
+            is ProtocolNegotiationFailed -> throw Libp2pException("ProtocolNegotiationFailed: $evt")
+        }
+        super.userEventTriggered(ctx, evt)
+    }
+
+    override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable?) {
+        ctx.close()
+    }
+
+    override fun channelUnregistered(ctx: ChannelHandlerContext) {
+        selectedFuture.completeExceptionally(ConnectionClosedException("Channel closed ${ctx.channel()}"))
+    }
+}
