05e3e589730eb84aedad7ac179101ef1bf2168fc
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-08-29 17:58:06 +0300

    Add Host and Network interfaces

--- src/main/kotlin/io/libp2p/core/Host.kt
+++ src/main/kotlin/io/libp2p/core/Host.kt
@@ -1,15 +1,39 @@
 package io.libp2p.core
 
 import io.libp2p.core.crypto.PrivKey
+import io.libp2p.core.multiformats.Multiaddr
+import io.libp2p.core.multistream.ProtocolMatcher
 import java.util.concurrent.CompletableFuture
 
 /**
  * The Host is the libp2p entrypoint. It is tightly coupled with all its inner components right now; in the near future
  * it should use some kind of dependency injection to wire itself.
  */
-class Host(
+interface Host {
+    val privKey: PrivKey
+    val peerId: PeerId
+    val network: NetworkImpl
+    val addressBook: AddressBook
+
+    val streams: Map<Connection, Stream>
+
+    fun start(): CompletableFuture<Unit>
+    fun stop(): CompletableFuture<Unit>
+
+    fun addStreamHandler(protocol: ProtocolMatcher, handler: StreamHandler): Unit = TODO()
+    fun removeStreamHandler(protocol: ProtocolMatcher): Unit = TODO()
+
+    fun addConnectionHandler(handler: ConnectionHandler): Unit = TODO()
+    fun removeConnectionHandler(handler: ConnectionHandler): Unit = TODO()
+
+    fun newStream(conn: Connection, protocol: String): CompletableFuture<Stream> = TODO()
+    fun newStream(addr: Multiaddr, protocol: String): CompletableFuture<Stream> = TODO()
+}
+
+
+class HostImpl(
     val privKey: PrivKey,
-    val network: Network,
+    val network: NetworkImpl,
     val addressBook: AddressBook
 ) {
 
--- src/main/kotlin/io/libp2p/core/Network.kt
+++ src/main/kotlin/io/libp2p/core/Network.kt
@@ -9,7 +9,21 @@ import java.util.concurrent.ConcurrentHashMap
 /**
  * The networkConfig component handles all networkConfig affairs, particularly listening on endpoints and dialing peers.
  */
-class Network(
+interface Network {
+    val transports: List<Transport>
+    val connectionHandler: ConnectionHandler
+    val streamHandler: StreamHandler
+
+    val connections: List<Connection>
+
+    fun listen(addr: Multiaddr): CompletableFuture<Unit>
+    fun unlisten(addr: Multiaddr): CompletableFuture<Unit>
+
+    fun connect(id: PeerId, vararg addrs: Multiaddr): CompletableFuture<Connection>
+    fun disconnect(conn: Connection): CompletableFuture<Unit>
+}
+
+class NetworkImpl(
     private val transports: List<Transport>,
     private val config: Config
 ) {
