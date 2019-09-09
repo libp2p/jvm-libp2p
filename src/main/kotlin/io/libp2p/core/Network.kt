8551769baaedeb3ad05a9d58c11785a426f03178
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-09-09 21:30:11 +0300

    Add Network.connect() variant where peerId is embedded as /p2p protocol

--- src/main/kotlin/io/libp2p/core/Network.kt
+++ src/main/kotlin/io/libp2p/core/Network.kt
@@ -1,6 +1,7 @@
 package io.libp2p.core
 
 import io.libp2p.core.multiformats.Multiaddr
+import io.libp2p.core.multiformats.Protocol
 import io.libp2p.core.transport.Transport
 import java.util.concurrent.CompletableFuture
 
@@ -15,6 +16,15 @@ interface Network {
 
     fun listen(addr: Multiaddr): CompletableFuture<Unit>
     fun unlisten(addr: Multiaddr): CompletableFuture<Unit>
+
+    fun connect(vararg addrs: Multiaddr): CompletableFuture<Connection> {
+        val peerIdSet = addrs.map {
+            it.getStringComponents().find { it.first == Protocol.P2P }?.second
+                ?: throw Libp2pException("Multiaddress should contain /p2p/<peerId> component")
+        }.toSet()
+        if (peerIdSet.size != 1) throw Libp2pException("All multiaddresses should nave the same peerId")
+        return connect(PeerId.fromBase58(peerIdSet.first()))
+    }
 
     fun connect(id: PeerId, vararg addrs: Multiaddr): CompletableFuture<Connection>
     fun disconnect(conn: Connection): CompletableFuture<Unit>
