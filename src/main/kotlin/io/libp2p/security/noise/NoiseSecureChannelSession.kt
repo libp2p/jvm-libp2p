36cbaae4fc3db68ebd2945730b956359ddbe08bb
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2019-09-16 20:50:24 +0300

    Embed properties to class constructor

--- src/main/kotlin/io/libp2p/security/noise/NoiseSecureChannelSession.kt
+++ src/main/kotlin/io/libp2p/security/noise/NoiseSecureChannelSession.kt
@@ -5,7 +5,10 @@ import io.libp2p.core.PeerId
 import io.libp2p.core.crypto.PubKey
 import io.libp2p.core.security.SecureChannel
 
-class NoiseSecureChannelSession(localId: PeerId, remoteId: PeerId, remotePubKey: PubKey, aliceCipher: CipherState, bobCipher: CipherState) : SecureChannel.Session(localId, remoteId, remotePubKey) {
-    val aliceCipher = aliceCipher
-    val bobCipher = bobCipher
-}
+class NoiseSecureChannelSession(
+    localId: PeerId,
+    remoteId: PeerId,
+    remotePubKey: PubKey,
+    val aliceCipher: CipherState,
+    val bobCipher: CipherState
+) : SecureChannel.Session(localId, remoteId, remotePubKey)
