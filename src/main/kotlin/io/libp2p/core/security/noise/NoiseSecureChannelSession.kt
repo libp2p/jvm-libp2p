18b52a2ef6e9e2139f5653bd659c4d6ca5f1b4ed
Shahan Khatchadourian <shahan.k.code@gmail.com>
2019-09-15 23:55:43 -0400

    Added imports

--- src/main/kotlin/io/libp2p/core/security/noise/NoiseSecureChannelSession.kt
+++ src/main/kotlin/io/libp2p/core/security/noise/NoiseSecureChannelSession.kt
@@ -1,5 +1,10 @@
 package io.libp2p.core.security.noise
 
+import com.southernstorm.noise.protocol.CipherState
+import io.libp2p.core.PeerId
+import io.libp2p.core.crypto.PubKey
+import io.libp2p.core.security.SecureChannel
+
 class NoiseSecureChannelSession(localId: PeerId, remoteId: PeerId, remotePubKey: PubKey, aliceCipher: CipherState, bobCipher: CipherState): SecureChannel.Session(localId, remoteId, remotePubKey) {
     val aliceCipher = aliceCipher
     val bobCipher = bobCipher
