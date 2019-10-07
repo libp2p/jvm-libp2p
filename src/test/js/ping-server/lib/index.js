fc0f2cfc7e90604603b075012218ee0a8b82a9c6
Jez Higgins <jez@jezuk.co.uk>
2019-10-07 11:02:09 +0100

    Stripped JS right down

---
+++ src/test/js/ping-server/lib/index.js
@@ -0,0 +1,63 @@
+const multiaddr = require('multiaddr')
+const PeerInfo = require('peer-info')
+const PeerId = require('peer-id')
+const Libp2p = require('libp2p')
+const TCP = require('libp2p-tcp')
+const Multiplex = require('libp2p-mplex')
+const SECIO = require('libp2p-secio')
+
+const defaultsDeep = require('@nodeutils/defaults-deep')
+
+const DEFAULT_OPTS = {
+  modules: {
+    transport: [
+      TCP
+    ],
+    connEncryption: [
+      SECIO
+    ],
+    streamMuxer: [
+      Multiplex
+    ]
+  }
+}
+
+class PingNode extends Libp2p {
+  constructor (opts) {
+    super(defaultsDeep(opts, DEFAULT_OPTS))
+  }
+
+}
+
+function createPingServer(callback) {
+  PeerInfo.create((err, peerInfo) => {
+    if (err) return callback(err)
+
+    const listenAddress = multiaddr('/ip4/127.0.0.1/tcp/40000')
+    peerInfo.multiaddrs.add(listenAddress)
+
+    const server = new Libp2p(defaultsDeep(
+        {peerInfo},
+        DEFAULT_OPTS
+    ))
+
+    server.on('error', err => {
+      console.error('libp2p error: ', err)
+      throw err
+    })
+
+    callback(null, server)
+  })
+}
+
+createPingServer((err, server) => {
+  if (err) throw err
+
+  server.start(err => {
+    if (err) throw err
+
+    const addresses = server.peerInfo.multiaddrs.toArray()
+    console.log('Ping server started.\n  Listening on addresses:')
+    addresses.forEach(addr => console.log(`    ${addr}`))
+  })
+})
