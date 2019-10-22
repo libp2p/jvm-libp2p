3a601796772a13652a51e1a341cfabe4692bfbb9
Jez Higgins <jez@jezuk.co.uk>
2019-10-22 11:22:11 +0100

    Define do nothing test protocol, and use that for our "unknown protocol" tests.

---
+++ src/test/kotlin/io/libp2p/tools/DoNothingProtocol.kt
@@ -0,0 +1,29 @@
+package io.libp2p.tools
+
+import io.libp2p.core.Stream
+import io.libp2p.core.multistream.StrictProtocolBinding
+import io.libp2p.protocol.ProtocolHandler
+import io.libp2p.protocol.ProtocolMessageHandler
+import io.netty.buffer.ByteBuf
+import java.util.concurrent.CompletableFuture
+
+interface DoNothingController
+
+class DoNothing : DoNothingBinding(DoNothingProtocol())
+
+open class DoNothingBinding(nullProtocol: DoNothingProtocol) : StrictProtocolBinding<DoNothingController>(nullProtocol) {
+    override val announce = "/ipfs/do-nothing/1.0.0"
+}
+
+class DoNothingProtocol : ProtocolHandler<DoNothingController>() {
+    override fun onStartInitiator(stream: Stream) = addDoNothingHandler(stream)
+    override fun onStartResponder(stream: Stream) = addDoNothingHandler(stream)
+
+    private fun addDoNothingHandler(stream: Stream): CompletableFuture<DoNothingController> {
+        val handler = DoNothingHandler()
+        stream.pushHandler(handler)
+        return CompletableFuture.completedFuture(handler)
+    }
+
+    inner class DoNothingHandler : ProtocolMessageHandler<ByteBuf>, DoNothingController
+}
