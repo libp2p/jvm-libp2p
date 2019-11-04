0864de99c0ded7fce73081a3a8e2413a64bc7244
Jez Higgins <jez@jezuk.co.uk>
2019-11-04 10:10:57 +0000

    SecIoHandshake doesn't need to be an inner class

--- src/main/kotlin/io/libp2p/security/secio/SecIoSecureChannel.kt
+++ src/main/kotlin/io/libp2p/security/secio/SecIoSecureChannel.kt
@@ -20,17 +20,12 @@ import io.netty.handler.codec.LengthFieldPrepender
 import org.apache.logging.log4j.LogManager
 import java.util.concurrent.CompletableFuture
 
-class SecIoSecureChannel(val localKey: PrivKey) :
-    SecureChannel {
+private val log = LogManager.getLogger(SecIoSecureChannel::class.java)
+private val HandshakeHandlerName = "SecIoHandshake"
 
-    private val log = LogManager.getLogger(SecIoSecureChannel::class.java)
-
-    private val HandshakeHandlerName = "SecIoHandshake"
-    private val HadshakeTimeout = 30 * 1000L
-
+class SecIoSecureChannel(private val localKey: PrivKey) : SecureChannel {
     override val announce = "/secio/1.0.0"
-    override val matcher =
-        ProtocolMatcher(Mode.STRICT, name = "/secio/1.0.0")
+    override val matcher = ProtocolMatcher(Mode.STRICT, name = "/secio/1.0.0")
 
     override fun initChannel(ch: P2PChannel, selectedProtocol: String): CompletableFuture<SecureChannel.Session> {
         val handshakeComplete = CompletableFuture<SecureChannel.Session>()
@@ -38,66 +33,69 @@ class SecIoSecureChannel(val localKey: PrivKey) :
         listOf(
             "PacketLenEncoder" to LengthFieldPrepender(4),
             "PacketLenDecoder" to LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4),
-            HandshakeHandlerName to SecIoHandshake(handshakeComplete)
+            HandshakeHandlerName to SecIoHandshake(localKey, handshakeComplete)
         ).forEach { ch.pushHandler(it.first, it.second) }
 
         return handshakeComplete
     }
+} // class SecIoSecureChannel
 
-    inner class SecIoHandshake(private val handshakeComplete: CompletableFuture<SecureChannel.Session>)
-            : SimpleChannelInboundHandler<ByteBuf>() {
-        private lateinit var negotiator: SecIoNegotiator
-        private var activated = false
-        private lateinit var secIoCodec: SecIoCodec
+private class SecIoHandshake(
+    private val localKey: PrivKey,
+    private val handshakeComplete: CompletableFuture<SecureChannel.Session>
+) : SimpleChannelInboundHandler<ByteBuf>() {
+    private lateinit var negotiator: SecIoNegotiator
+    private var activated = false
+    private lateinit var secIoCodec: SecIoCodec
 
-        override fun channelActive(ctx: ChannelHandlerContext) {
-            if (!activated) {
-                activated = true
-                val remotePeerId = ctx.channel().attr(REMOTE_PEER_ID).get()
-                negotiator =
-                    SecIoNegotiator({ buf -> writeAndFlush(ctx, buf) }, localKey, remotePeerId)
-                negotiator.start()
-            }
-        }
-
-        override fun channelRead0(ctx: ChannelHandlerContext, msg: ByteBuf) {
-            // it seems there is no guarantee from Netty that channelActive() must be called before channelRead()
-            channelActive(ctx)
-
-            val keys = negotiator.onNewMessage(msg)
-
-            if (keys != null) {
-                secIoCodec = SecIoCodec(keys.first, keys.second)
-                ctx.channel().pipeline().addBefore(HandshakeHandlerName, "SecIoCodec", secIoCodec)
-                negotiator.onSecureChannelSetup()
-            }
-
-            if (negotiator.isComplete()) {
-                val session = SecureChannel.Session(
-                    PeerId.fromPubKey(secIoCodec.local.permanentPubKey),
-                    PeerId.fromPubKey(secIoCodec.remote.permanentPubKey),
-                    secIoCodec.remote.permanentPubKey
-                )
-                handshakeComplete.complete(session)
-                ctx.channel().pipeline().remove(HandshakeHandlerName)
-                ctx.fireChannelActive()
-            }
-        }
-
-        private fun writeAndFlush(ctx: ChannelHandlerContext, bb: ByteBuf) {
-            ctx.writeAndFlush(bb)
-        }
-
-        override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
-            handshakeComplete.completeExceptionally(cause)
-            log.error(cause.message)
-            ctx.channel().close()
-        }
-
-        override fun channelUnregistered(ctx: ChannelHandlerContext) {
-            handshakeComplete.completeExceptionally(ConnectionClosedException("Connection was closed ${ctx.channel()}"))
-            super.channelUnregistered(ctx)
+    override fun channelActive(ctx: ChannelHandlerContext) {
+        if (!activated) {
+            activated = true
+            val remotePeerId = ctx.channel().attr(REMOTE_PEER_ID).get()
+            negotiator =
+                SecIoNegotiator({ buf -> writeAndFlush(ctx, buf) }, localKey, remotePeerId)
+            negotiator.start()
         }
     }
+
+    override fun channelRead0(ctx: ChannelHandlerContext, msg: ByteBuf) {
+        // it seems there is no guarantee from Netty that channelActive() must be called before channelRead()
+        channelActive(ctx)
+
+        val keys = negotiator.onNewMessage(msg)
+
+        if (keys != null) {
+            secIoCodec = SecIoCodec(keys.first, keys.second)
+            ctx.channel().pipeline().addBefore(HandshakeHandlerName, "SecIoCodec", secIoCodec)
+            negotiator.onSecureChannelSetup()
+        }
+
+        if (negotiator.isComplete()) {
+            val session = SecureChannel.Session(
+                PeerId.fromPubKey(secIoCodec.local.permanentPubKey),
+                PeerId.fromPubKey(secIoCodec.remote.permanentPubKey),
+                secIoCodec.remote.permanentPubKey
+            )
+            handshakeComplete.complete(session)
+            ctx.channel().pipeline().remove(HandshakeHandlerName)
+            ctx.fireChannelActive()
+        }
+    }
+
+    private fun writeAndFlush(ctx: ChannelHandlerContext, bb: ByteBuf) {
+        ctx.writeAndFlush(bb)
+    }
+
+    override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
+        handshakeComplete.completeExceptionally(cause)
+        log.error(cause.message)
+        ctx.channel().close()
+    }
+
+    override fun channelUnregistered(ctx: ChannelHandlerContext) {
+        handshakeComplete.completeExceptionally(ConnectionClosedException("Connection was closed ${ctx.channel()}"))
+        super.channelUnregistered(ctx)
+    }
 }
+
 
