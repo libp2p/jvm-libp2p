8d6be0aabc3d4af2398ec7663705354b9a48768d
Jez Higgins <jez@jezuk.co.uk>
2019-11-11 16:32:05 +0000

    Start of WsTransport.listen

---
+++ src/main/kotlin/io/libp2p/transport/ws/WebSocketServerInitializer.kt
@@ -0,0 +1,21 @@
+package io.libp2p.transport.ws
+
+import io.netty.channel.ChannelInitializer
+import io.netty.channel.socket.SocketChannel
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler
+import io.netty.handler.codec.http.HttpObjectAggregator
+import io.netty.handler.codec.http.HttpServerCodec
+
+class WebSocketServerInitializer :
+        ChannelInitializer<SocketChannel>() {
+
+    public override fun initChannel(ch: SocketChannel) {
+        val pipeline = ch.pipeline()
+
+        pipeline.addLast(HttpServerCodec())
+        pipeline.addLast(HttpObjectAggregator(65536))
+        pipeline.addLast(WebSocketServerCompressionHandler())
+        pipeline.addLast(WebSocketServerProtocolHandler("/", null, true))
+    } // initChannel
+} // WebSocketServerInitializer
--- src/main/kotlin/io/libp2p/transport/ws/WsTransport.kt
+++ src/main/kotlin/io/libp2p/transport/ws/WsTransport.kt
@@ -3,16 +3,42 @@ package io.libp2p.transport.ws
 import io.libp2p.core.Connection
 import io.libp2p.core.ConnectionHandler
 import io.libp2p.core.Libp2pException
+import io.libp2p.core.PeerId
 import io.libp2p.core.multiformats.Multiaddr
 import io.libp2p.core.multiformats.Protocol
 import io.libp2p.core.transport.Transport
+import io.libp2p.etc.CONNECTION
+import io.libp2p.etc.REMOTE_PEER_ID
+import io.libp2p.etc.types.forward
+import io.libp2p.etc.types.lazyVar
+import io.libp2p.etc.types.toVoidCompletableFuture
+import io.libp2p.etc.util.netty.nettyInitializer
 import io.libp2p.transport.ConnectionUpgrader
+import io.libp2p.transport.tcp.ConnectionOverNetty
+import io.netty.bootstrap.ServerBootstrap
+import io.netty.channel.Channel
+import io.netty.channel.ChannelHandler
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import java.net.InetSocketAddress
 import java.util.concurrent.CompletableFuture
 
 class WsTransport(
     val upgrader: ConnectionUpgrader
 ) : Transport {
     private var closed = false
+    val activeListeners = mutableMapOf<Multiaddr, Channel>()
+
+    // server Bootstrap prototype
+    private var workerGroup by lazyVar { NioEventLoopGroup() }
+    private var bossGroup by lazyVar { workerGroup }
+
+    private var server by lazyVar {
+        ServerBootstrap().apply {
+            group(bossGroup, workerGroup)
+            channel(NioServerSocketChannel::class.java)
+        }
+    }
 
     override fun initialize() {
     }
@@ -26,14 +52,32 @@ class WsTransport(
     override fun close(): CompletableFuture<Unit> {
         closed = true
 
-        return CompletableFuture.completedFuture(null)
-    }
+        val unbindsCompleted = activeListeners
+            .map { (_, ch) -> ch }
+            .map { it.close().toVoidCompletableFuture() }
+
+        return CompletableFuture.allOf(*unbindsCompleted.toTypedArray()).thenApply { }
+    } // close
 
     override fun listen(addr: Multiaddr, connHandler: ConnectionHandler): CompletableFuture<Unit> {
         if (closed) throw Libp2pException("Transport is closed")
 
-        TODO("not implemented")
-    }
+        val listener = server.clone()
+            .childHandler(WebSocketServerInitializer())
+
+        val bindComplete = listener.bind(fromMultiaddr(addr))
+
+        bindComplete.also {
+            activeListeners += addr to it.channel()
+            it.channel().closeFuture().addListener {
+                synchronized(this@WsTransport) {
+                    activeListeners -= addr
+                }
+            }
+        }
+
+        return bindComplete.toVoidCompletableFuture()
+    } // listener
 
     override fun unlisten(addr: Multiaddr): CompletableFuture<Unit> {
         TODO("not implemented")
@@ -43,5 +87,17 @@ class WsTransport(
         if (closed) throw Libp2pException("Transport is closed")
 
         TODO("not implemented")
+    }
+
+    private fun fromMultiaddr(addr: Multiaddr): InetSocketAddress {
+        val host = addr.filterStringComponents().find { p -> p.first in arrayOf(
+            Protocol.IP4,
+            Protocol.IP6,
+            Protocol.DNSADDR
+        ) }
+            ?.second ?: throw Libp2pException("Missing IP4/IP6/DNSADDR in multiaddress $addr")
+        val port = addr.filterStringComponents().find { p -> p.first == Protocol.TCP }
+            ?.second ?: throw Libp2pException("Missing TCP in multiaddress $addr")
+        return InetSocketAddress(host, port.toInt())
     }
 }
