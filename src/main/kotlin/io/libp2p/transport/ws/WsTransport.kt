b1811fba7c0aed9a66213caed010d69a91de5c14
Jez Higgins <jez@jezuk.co.uk>
2019-11-11 15:26:34 +0000

    Initial do-nothing WebSocket transport

--- src/main/kotlin/io/libp2p/transport/ws/WsTransport.kt
+++ src/main/kotlin/io/libp2p/transport/ws/WsTransport.kt
@@ -3,6 +3,7 @@ package io.libp2p.transport.ws
 import io.libp2p.core.Connection
 import io.libp2p.core.ConnectionHandler
 import io.libp2p.core.multiformats.Multiaddr
+import io.libp2p.core.multiformats.Protocol
 import io.libp2p.core.transport.Transport
 import io.libp2p.transport.ConnectionUpgrader
 import java.util.concurrent.CompletableFuture
