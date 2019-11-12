5508278d0e2000ff6a1a92c32369b8321e3e69cf
Jez Higgins <jez@jezuk.co.uk>
2019-11-12 14:59:15 +0000

    Rework TcpTransport to use NettyTransportBase. Fixes race condition in Transport.close

--- src/main/kotlin/io/libp2p/transport/tcp/TcpTransport.kt
+++ src/main/kotlin/io/libp2p/transport/tcp/TcpTransport.kt
@@ -1,37 +1,14 @@
 package io.libp2p.transport.tcp
 
-import io.libp2p.core.Connection
-import io.libp2p.core.ConnectionHandler
-import io.libp2p.core.PeerId
-import io.libp2p.core.Libp2pException
 import io.libp2p.core.multiformats.Multiaddr
-import io.libp2p.core.multiformats.Protocol
 import io.libp2p.core.multiformats.Protocol.DNSADDR
 import io.libp2p.core.multiformats.Protocol.IP4
 import io.libp2p.core.multiformats.Protocol.IP6
 import io.libp2p.core.multiformats.Protocol.TCP
 import io.libp2p.core.multiformats.Protocol.WS
-import io.libp2p.core.transport.Transport
-import io.libp2p.etc.CONNECTION
-import io.libp2p.etc.REMOTE_PEER_ID
-import io.libp2p.etc.types.forward
-import io.libp2p.etc.types.lazyVar
-import io.libp2p.etc.types.toCompletableFuture
-import io.libp2p.etc.types.toVoidCompletableFuture
-import io.libp2p.etc.util.netty.nettyInitializer
 import io.libp2p.transport.ConnectionUpgrader
-import io.libp2p.transport.implementation.ConnectionOverNetty
-import io.netty.bootstrap.Bootstrap
-import io.netty.bootstrap.ServerBootstrap
-import io.netty.channel.Channel
+import io.libp2p.transport.implementation.NettyTransportBase
 import io.netty.channel.ChannelHandler
-import io.netty.channel.ChannelOption
-import io.netty.channel.nio.NioEventLoopGroup
-import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.channel.socket.nio.NioSocketChannel
-import java.net.InetSocketAddress
-import java.time.Duration
-import java.util.concurrent.CompletableFuture
 
 /**
  * The TCP transport can establish libp2p connections via TCP endpoints.
@@ -40,145 +17,16 @@ import java.util.concurrent.CompletableFuture
  * shim those capabilities via dynamic negotiation.
  */
 class TcpTransport(
-    val upgrader: ConnectionUpgrader
-) : Transport {
-    var workerGroup by lazyVar { NioEventLoopGroup() }
-    var bossGroup by lazyVar { workerGroup }
-    var connectTimeout = Duration.ofSeconds(15)
-
-    private val listeners = mutableMapOf<Multiaddr, Channel>()
-    private val channels = mutableListOf<Channel>()
-    private var closed = false
-
-    // client Bootstrap prototype
-    var client by lazyVar {
-        Bootstrap().apply {
-            group(workerGroup)
-            channel(NioSocketChannel::class.java)
-            option(ChannelOption.CONNECT_TIMEOUT_MILLIS, connectTimeout.toMillis().toInt())
-        }
-    }
-
-    // server Bootstrap prototype
-    var server by lazyVar {
-        ServerBootstrap().apply {
-            group(bossGroup, workerGroup)
-            channel(NioServerSocketChannel::class.java)
-        }
-    }
-
-    // Initializes the server and client fields, preparing them to establish outbound connections (client)
-    // and to accept inbound connections (server).
-    override val activeListeners: Int
-        get() = listeners.size
-    override val activeConnections: Int
-        get() = channels.size
-
-    override fun initialize() {
-    }
-
+    upgrader: ConnectionUpgrader
+) : NettyTransportBase(upgrader) {
     // Checks if this transport can handle this multiaddr. It should return true for multiaddrs containing `tcp` atoms.
     override fun handles(addr: Multiaddr): Boolean {
         return (addr.has(IP4) || addr.has(IP6) || addr.has(DNSADDR)) &&
                 addr.has(TCP) &&
                 !addr.has(WS)
-    }
+    } // handles
 
-    // Closes this transport entirely, aborting all ongoing connections and shutting down any listeners.
-    @Synchronized
-    override fun close(): CompletableFuture<Unit> {
-        closed = true
-        val unbindFutures = listeners
-            .map { (_, ch) -> ch }
-            .map { it.close().toVoidCompletableFuture() }
-        return CompletableFuture.allOf(*unbindFutures.toTypedArray())
-            .thenCompose {
-                synchronized(this@TcpTransport) {
-                    val closeFutures = channels.toMutableList()
-                        .map { it.close().toVoidCompletableFuture() }
-                    CompletableFuture.allOf(*closeFutures.toTypedArray())
-                }
-            }.thenApply { }
-    }
+    override fun serverInitializer(addr: Multiaddr): ChannelHandler? = null
 
-    @Synchronized
-    override fun listen(addr: Multiaddr, connHandler: ConnectionHandler): CompletableFuture<Unit> {
-        if (closed) throw Libp2pException("Transport is closed")
-        return server.clone()
-            .childHandler(nettyInitializer { ch ->
-                registerChannel(ch)
-                val (channelHandler, _) = createConnectionHandler(connHandler, false)
-                ch.pipeline().addLast(channelHandler)
-            })
-            .bind(fromMultiaddr(addr))
-            .also { ch ->
-                listeners += addr to ch.channel()
-                ch.channel().closeFuture().addListener {
-                    synchronized(this@TcpTransport) {
-                        listeners -= addr
-                    }
-                }
-            }
-            .toVoidCompletableFuture()
-    }
-
-    @Synchronized
-    override fun unlisten(addr: Multiaddr): CompletableFuture<Unit> {
-        return listeners[addr]?.close()?.toVoidCompletableFuture()
-            ?: throw Libp2pException("No listeners on address $addr")
-    }
-
-    @Synchronized
-    override fun dial(addr: Multiaddr, connHandler: ConnectionHandler): CompletableFuture<Connection> {
-        if (closed) throw Libp2pException("Transport is closed")
-        val remotePeerId = addr.getStringComponent(Protocol.P2P)?.let { PeerId.fromBase58(it) }
-        val (channelHandler, connFuture) = createConnectionHandler(connHandler, true, remotePeerId)
-        return client.clone()
-            .handler(channelHandler)
-            .connect(fromMultiaddr(addr))
-            .also { registerChannel(it.channel()) }
-            .toCompletableFuture().thenCompose { connFuture }
-    }
-
-    @Synchronized
-    private fun registerChannel(ch: Channel): Channel {
-        if (closed) {
-            ch.close()
-        } else {
-            channels += ch
-            ch.closeFuture().addListener { channels -= ch }
-        }
-        return ch
-    }
-
-    private fun createConnectionHandler(
-        connHandler: ConnectionHandler,
-        initiator: Boolean,
-        remotePeerId: PeerId? = null
-    ): Pair<ChannelHandler, CompletableFuture<Connection>> {
-        val connFuture = CompletableFuture<Connection>()
-        return nettyInitializer { ch ->
-            val connection = ConnectionOverNetty(ch, this, initiator)
-            ch.attr(CONNECTION).set(connection)
-            remotePeerId?.also { ch.attr(REMOTE_PEER_ID).set(it) }
-            upgrader.establishSecureChannel(connection)
-                .thenCompose {
-                    connection.setSecureSession(it)
-                    upgrader.establishMuxer(connection)
-                }.thenApply {
-                    connection.setMuxerSession(it)
-                    connHandler.handleConnection(connection)
-                    connection
-                }
-                .forward(connFuture)
-        } to connFuture
-    }
-
-    private fun fromMultiaddr(addr: Multiaddr): InetSocketAddress {
-        val host = addr.filterStringComponents().find { p -> p.first in arrayOf(IP4, IP6, DNSADDR) }
-            ?.second ?: throw Libp2pException("Missing IP4/IP6/DNSADDR in multiaddress $addr")
-        val port = addr.filterStringComponents().find { p -> p.first == TCP }
-            ?.second ?: throw Libp2pException("Missing TCP in multiaddress $addr")
-        return InetSocketAddress(host, port.toInt())
-    }
-}
+    override fun clientInitializer(addr: Multiaddr): ChannelHandler? = null
+} // class TcpTransport
