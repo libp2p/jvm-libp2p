09e4abcd23a81923fd06b041d143baa41c22df57
Jez Higgins <jez@jezuk.co.uk>
2019-11-12 10:29:37 +0000

    Do nothing connection upgrader shim for use when testing transports

---
+++ src/test/kotlin/io/libp2p/transport/NullConnectionUpgrader.kt
@@ -0,0 +1,38 @@
+package io.libp2p.transport
+
+import io.libp2p.core.Connection
+import io.libp2p.core.PeerId
+import io.libp2p.core.StreamHandler
+import io.libp2p.core.StreamPromise
+import io.libp2p.core.crypto.KEY_TYPE
+import io.libp2p.core.crypto.generateKeyPair
+import io.libp2p.core.mux.StreamMuxer
+import io.libp2p.core.security.SecureChannel
+import java.util.concurrent.CompletableFuture
+
+class NullConnectionUpgrader:
+    ConnectionUpgrader(emptyList(), emptyList()) {
+
+    override fun establishSecureChannel(connection: Connection): CompletableFuture<SecureChannel.Session> {
+        val nonsenseSession = SecureChannel.Session(
+            PeerId.random(),
+            PeerId.random(),
+            generateKeyPair(KEY_TYPE.RSA).second
+        )
+        return CompletableFuture.completedFuture(nonsenseSession)
+    } // establishSecureChannel
+
+    override fun establishMuxer(connection: Connection): CompletableFuture<StreamMuxer.Session> {
+        return CompletableFuture.completedFuture(DoNothingMuxerSession())
+    } // establishMuxer
+
+    private class DoNothingMuxerSession : StreamMuxer.Session {
+        override var inboundStreamHandler: StreamHandler<*>?
+            get() = throw NotImplementedError("Test only. Shouldn't be called")
+            set(value) {}
+
+        override fun <T> createStream(streamHandler: StreamHandler<T>): StreamPromise<T> {
+            throw NotImplementedError("Test only. Shouldn't be called")
+        }
+    }
+} // class NullConnectionUpgrader
