2b5f0b1a7eb50c81011d3dd28b550698ab6ca0ec
Jez Higgins <jez@jezuk.co.uk>
2019-11-25 13:01:42 +0000

    Extend Multiaddr to all straightforward creation of Multiaddrs with PeerIds

--- src/main/kotlin/io/libp2p/core/multiformats/Multiaddr.kt
+++ src/main/kotlin/io/libp2p/core/multiformats/Multiaddr.kt
@@ -1,5 +1,6 @@
 package io.libp2p.core.multiformats
 
+import io.libp2p.core.PeerId
 import io.libp2p.etc.types.readUvarint
 import io.libp2p.etc.types.toByteArray
 import io.libp2p.etc.types.toByteBuf
@@ -20,7 +21,6 @@ import io.netty.buffer.Unpooled
  * to this protocol rule
  */
 class Multiaddr(val components: List<Pair<Protocol, ByteArray>>) {
-
     /**
      * Creates instance from the string representation
      */
@@ -34,6 +34,9 @@ class Multiaddr(val components: List<Pair<Protocol, ByteArray>>) {
      * Creates instance from serialized form from [ByteBuf]
      */
     constructor(bytes: ByteArray) : this(parseBytes(bytes.toByteBuf()))
+
+    constructor(parentAddr: Multiaddr, peerId: PeerId) :
+            this(concatProtocols(parentAddr, peerId))
 
     /**
      * Returns only components matching any of supplied protocols
@@ -145,6 +148,14 @@ class Multiaddr(val components: List<Pair<Protocol, ByteArray>>) {
                 ret.add(protocol to protocol.readAddressBytes(buf))
             }
             return ret
+        }
+
+        private fun concatProtocols(addr: Multiaddr, peerId: PeerId): List<Pair<Protocol, ByteArray>> {
+            if (addr.has(Protocol.IPFS))
+                throw IllegalArgumentException("Multiaddr already has peer id")
+            val protocols = addr.components.toMutableList()
+            protocols.add(Pair(Protocol.IPFS, peerId.bytes))
+            return protocols
         }
     }
 }
