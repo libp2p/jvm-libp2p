06a1af0432577f61bfa0772622dc7b7d543990c2
Jez Higgins <jez@jezuk.co.uk>
2019-12-05 15:54:39 +0000

    Simple DNS4 resolution

--- src/main/kotlin/io/libp2p/core/multiformats/MultiaddrDns.kt
+++ src/main/kotlin/io/libp2p/core/multiformats/MultiaddrDns.kt
@@ -1,5 +1,8 @@
 package io.libp2p.core.multiformats
 
+import io.libp2p.etc.types.copy
+import java.net.Inet4Address
+
 class MultiaddrDns {
     companion object {
         private val dnsProtocols = arrayOf(Protocol.DNS4, Protocol.DNS6, Protocol.DNSADDR)
@@ -24,7 +27,22 @@ class MultiaddrDns {
         }
 
         private fun resolve(proto: Protocol, hostname: String, address: Multiaddr): List<Multiaddr> {
-            return listOf(address)
+            return when (proto) {
+                Protocol.DNS4 -> resolveDns4(hostname, address)
+                else -> {
+                    TODO(proto.toString() + " not done yet")
+                }
+            }
+        }
+
+        private fun resolveDns4(hostname: String, address: Multiaddr): List<Multiaddr> {
+            val ipAddresses = Inet4Address.getAllByName(hostname)
+            return ipAddresses.filterIsInstance<Inet4Address>()
+                .map {
+                    val components = address.components.toMutableList()
+                    components[0] = Pair(Protocol.IP4, it.address)
+                    Multiaddr(components)
+                }
         }
 
         private fun crossProduct(addressMatrix: List<List<Multiaddr>>): List<Multiaddr> {
