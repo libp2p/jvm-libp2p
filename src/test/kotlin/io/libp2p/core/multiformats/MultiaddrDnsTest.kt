6febd19218952a87c2f453e7cdbf35df20387204
Jez Higgins <jez@jezuk.co.uk>
2019-12-05 16:49:32 +0000

    Verify multiaddr dns when host resolves to multiple addresses

--- src/test/kotlin/io/libp2p/core/multiformats/MultiaddrDnsTest.kt
+++ src/test/kotlin/io/libp2p/core/multiformats/MultiaddrDnsTest.kt
@@ -36,7 +36,30 @@ class MultiaddrDnsTest {
         assertEquals(Multiaddr(expected), resolved[0])
     }
 
+    @ParameterizedTest
+    @MethodSource("dnsResolvesToMultipleAddresses")
+    fun `hostname resolves to multiple addresses`(addr: String, expected: List<String>) {
+        val multiaddr = Multiaddr(addr)
+        val expectedMultiaddr = expected.map { Multiaddr(it) }
+
+        val resolved = MultiaddrDns.resolve(multiaddr, TestResolver)
+        assertEquals(expected.size, resolved.size)
+        assertEquals(expectedMultiaddr, resolved)
+    }
+
     companion object {
+        @JvmStatic
+        fun dnsResolvesToMultipleAddresses() = listOf(
+            Arguments.of(
+                "/dns4/pig.com",
+                listOf("/ip4/1.1.1.1", "/ip4/1.1.1.2")
+            ),
+            Arguments.of(
+                "/dns4/pig.com/tcp/8000",
+                listOf("/ip4/1.1.1.1/tcp/8000", "/ip4/1.1.1.2/tcp/8000")
+            )
+        )
+
         @JvmStatic
         fun dns4Localhost() = listOf(
             Arguments.of(
@@ -93,5 +116,22 @@ class MultiaddrDnsTest {
             "/ip4/127.0.0.1/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC",
             "/ip4/127.0.0.1/p2p/QmcgpsyWgH8Y8ajJz1Cu72KnS5uo2Aa2LpzU7kinSupNKC/tcp/1234"
         )
+
+        val TestResolver = object : MultiaddrDns.Resolver {
+            override fun resolveDns4(hostname: String): List<Multiaddr> {
+                val address = if ("pig.com".equals(hostname))
+                    listOf("/ip4/1.1.1.1", "/ip4/1.1.1.2")
+                else if ("localhost".equals(hostname))
+                    listOf("/ip4/127.0.0.1")
+                else
+                    listOf("/ip4/2.2.2.1", "/ip4/2.2.2.2")
+
+                return address.map { Multiaddr(it) }
+            }
+
+            override fun resolveDns6(hostname: String): List<Multiaddr> {
+                throw NotImplementedError("Not used")
+            }
+        }
     }
 }
