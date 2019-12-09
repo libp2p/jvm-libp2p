93a09be22de5dcf0f997ae44635ff790668733d5
Jez Higgins <jez@jezuk.co.uk>
2019-12-09 16:24:40 +0000

    Initial start/stop

--- src/main/kotlin/io/libp2p/discovery/MDnsDiscovery.kt
+++ src/main/kotlin/io/libp2p/discovery/MDnsDiscovery.kt
@@ -2,19 +2,27 @@ package io.libp2p.discovery
 
 import io.libp2p.core.PeerId
 import io.libp2p.core.PeerInfo
+import java.net.InetAddress
 import java.util.concurrent.CompletableFuture
+import javax.jmdns.JmDNS
 
 typealias PeerListener = (PeerInfo) -> Unit
 
 class MDnsDiscovery(
     private val peerId: PeerId
 ) {
+    private var mDns: JmDNS? = null
+
     fun start(): CompletableFuture<Void> {
-        TODO("stub")
+        mDns = JmDNS.create(InetAddress.getLocalHost())
+
+        return CompletableFuture.completedFuture(null)
     }
 
     fun stop(): CompletableFuture<Void> {
-        TODO("stub")
+        mDns?.unregisterAllServices()
+
+        return CompletableFuture.completedFuture(null)
     }
 
     fun onPeerFound(listener: PeerListener) { }
--- src/test/kotlin/io/libp2p/discovery/MDnsDiscoveryTest.kt
+++ src/test/kotlin/io/libp2p/discovery/MDnsDiscoveryTest.kt
@@ -1,22 +1,28 @@
 package io.libp2p.discovery
 
 import io.libp2p.core.PeerId
+import io.libp2p.core.PeerInfo
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.util.concurrent.TimeUnit
 
 class MDnsDiscoveryTest {
     private val peerId = PeerId.random()
 
     @Test
     fun startDiscoveryAndListenForSelf() {
+        var peerInfo: PeerInfo? = null
         val discoverer = MDnsDiscovery(peerId)
 
-        discoverer.onPeerFound({
-            discoverer.stop()
-            assertEquals(peerId, it.peerId)
-            assertEquals(1, it.addresses.size)
-        })
+        discoverer.onPeerFound {
+            peerInfo = it;
+        }
 
         discoverer.start()
+        TimeUnit.SECONDS.sleep(2)
+        discoverer.stop()
+
+        assertEquals(peerId, peerInfo?.peerId)
+        assertEquals(1, peerInfo?.addresses?.size)
     }
 }
