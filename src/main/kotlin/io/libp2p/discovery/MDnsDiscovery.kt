3e8a05f3b3f4aa39f05eafd2d09d1c1a348ce7f3
Jez Higgins <jez@jezuk.co.uk>
2019-12-10 15:06:07 +0000

    start querying and responding to service discovery requests

--- src/main/kotlin/io/libp2p/discovery/MDnsDiscovery.kt
+++ src/main/kotlin/io/libp2p/discovery/MDnsDiscovery.kt
@@ -5,25 +5,63 @@ import io.libp2p.core.PeerInfo
 import java.net.InetAddress
 import java.util.concurrent.CompletableFuture
 import javax.jmdns.JmDNS
+import javax.jmdns.ServiceEvent
+import javax.jmdns.ServiceInfo
+import javax.jmdns.ServiceListener
 
 typealias PeerListener = (PeerInfo) -> Unit
+
+class Listener : ServiceListener {
+    override fun serviceResolved(event: ServiceEvent?) {
+    }
+
+    override fun serviceRemoved(event: ServiceEvent?) {
+    }
+
+    override fun serviceAdded(event: ServiceEvent) {
+        println("Added")
+        println(event.toString())
+        println(event.info.toString())
+    }
+
+}
 
 class MDnsDiscovery(
     private val host: Host
 ) {
-    private var mDns: JmDNS? = null
+    private var mDns = JmDNS.create(InetAddress.getLocalHost())
 
     fun start(): CompletableFuture<Void> {
-        mDns = JmDNS.create(InetAddress.getLocalHost())
+        mDns.registerService(
+            ipfsDiscoveryInfo()
+        )
+        mDns.addServiceListener(
+            ServiceTagLocal,
+            Listener()
+        )
 
         return CompletableFuture.completedFuture(null)
     }
 
     fun stop(): CompletableFuture<Void> {
-        mDns?.unregisterAllServices()
+        mDns.unregisterAllServices()
 
         return CompletableFuture.completedFuture(null)
     }
 
     fun onPeerFound(listener: PeerListener) { }
+
+    private fun ipfsDiscoveryInfo(): ServiceInfo {
+        return ServiceInfo.create(
+            ServiceTagLocal,
+            host.peerId.toBase58(),
+            1234,
+            "hello mother"
+        )
+    }
+
+    companion object {
+        val ServiceTag = "_ipfs-discovery._udp"
+        val ServiceTagLocal = "${ServiceTag}.local."
+    }
 }
