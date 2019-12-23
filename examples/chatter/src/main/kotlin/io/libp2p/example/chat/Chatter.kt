b982bc95e29d4947572c193499caf757245c40a5
Jez Higgins <jez@jezuk.co.uk>
2019-12-23 11:09:23 +0000

    Start libp2p node

--- examples/chatter/src/main/kotlin/io/libp2p/example/chat/Chatter.kt
+++ examples/chatter/src/main/kotlin/io/libp2p/example/chat/Chatter.kt
@@ -1,5 +1,20 @@
 package io.libp2p.example.chat
 
+import io.libp2p.core.dsl.host
+import io.libp2p.protocol.Identify
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val chatHost = host {
+        protocols {
+            +Identify()
+        }
+        network {
+            listen("/ip4/127.0.0.1/tcp/0")
+        }
+    }
+
+    chatHost.start().get()
+
+    println("Libp2p Chatter!")
+    println("===============")
 }
