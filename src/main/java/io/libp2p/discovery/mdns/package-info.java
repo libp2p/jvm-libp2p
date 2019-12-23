3e6752d4ae6571c47128b18b3219c108b152a851
Jez Higgins <jez@jezuk.co.uk>
2019-12-23 08:29:18 +0000

    Added notes clarifying license status of JmDNS derived code.

--- src/main/java/io/libp2p/discovery/mdns/package-info.java
+++ src/main/java/io/libp2p/discovery/mdns/package-info.java
@@ -1,9 +1,26 @@
 package io.libp2p.discovery.mdns;
 
 /**
- * JmDNS is a Java implementation of multi-cast DNS and can be used for service registration and discovery in local area networks. JmDNS is fully compatible with Apple's Bonjour. The project was originally started in December 2002 by Arthur van Hoff
- * at Strangeberry. In November 2003 the project was moved to SourceForge, and the name was changed from JRendezvous to JmDNS for legal reasons. Many thanks to Stuart Cheshire for help and moral support.
+ * Java code in this package is derived from the JmDNS project.
+ *
+ * JmDNS is a Java implementation of multi-cast DNS and can be used for
+ * service registration and discovery in local area networks. JmDNS is
+ * fully compatible with Apple's Bonjour. The project was originally
+ * started in December 2002 by Arthur van Hoff at Strangeberry. In
+ * November 2003 the project was moved to SourceForge, and the name
+ * was changed from JRendezvous to JmDNS for legal reasons. Many
+ * thanks to Stuart Cheshire for help and moral support. In 2014, it was
+ * been moved from Sourceforge to Github by Kai Kreuzer with the kind
+ * approval from Arthur and Rick.
  * <p>
- * <a href="http://jmdns.sourceforge.net/">http://jmdns.sourceforge.net/<a>
+ * <a href="https://github.com/jmdns/jmdns/">https://github.com/jmdns/jmdns/</a>
+ * </p>
+ *
+ * JmDNS was originally licensed under the GNU Lesser General Public
+ * License as jRendevous. It was re-released under the
+ * Apache License, Version 2.0 in 2005. It is under those terms it is reused here.
+ * <p>
+ * <a href="https://github.com/jmdns/jmdns/blob/master/NOTICE.txt">JmDNS License Notice</a><br/>
+ * <a href="https://github.com/jmdns/jmdns/blob/d15acaf8ad5f11fc6a1dfa597e3e36803a332dbd/CHANGELOG.txt#L214">JmDNS Changelog</a>
  * </p>
  **/
