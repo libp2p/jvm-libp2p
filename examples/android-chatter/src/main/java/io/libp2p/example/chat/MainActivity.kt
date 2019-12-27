c48336d0a33e39747562a5a30d30e666b3c419bc
Jez Higgins <jez@jezuk.co.uk>
2019-12-27 19:37:34 +0000

    Trying to get Android multicast going. Maybe try IP6?

--- examples/android-chatter/src/main/java/io/libp2p/example/chat/MainActivity.kt
+++ examples/android-chatter/src/main/java/io/libp2p/example/chat/MainActivity.kt
@@ -1,17 +1,20 @@
 package io.libp2p.example.chat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.concurrent.CompletableFuture.runAsync
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var chatWindow: TextView
     private lateinit var line: EditText
     private lateinit var sendButton: Button
     private lateinit var chatNode: ChatNode
+    private var multicastLock: WifiManager.MulticastLock? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +26,21 @@ class MainActivity : AppCompatActivity() {
 
         sendButton.setOnClickListener { sendText() }
 
+        val wifi = getSystemService(WIFI_SERVICE) as WifiManager
+        multicastLock = wifi.createMulticastLock("multicastLock")
+        multicastLock?.acquire()
+
         runAsync {
             chatNode = ChatNode(::chatMessage)
             chatMessage("\nLibp2p Chatter!\n=============\n")
             chatMessage("This node is ${chatNode.peerId}, listening on ${chatNode.address}\n")
         }
+    }
+
+    override fun onDestroy() {
+        multicastLock?.release()
+        multicastLock = null
+        super.onDestroy()
     }
 
     private fun sendText() {
