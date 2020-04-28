08e99d1053bdcea4ac5850275de6660f0462d3d4
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-04-28 19:37:00 +0300

    Draft of Zero RT Multistream. Relates #94

--- src/main/kotlin/io/libp2p/etc/util/netty/StringSuffixCodec.kt
+++ src/main/kotlin/io/libp2p/etc/util/netty/StringSuffixCodec.kt
@@ -2,6 +2,8 @@ package io.libp2p.etc.util.netty
 
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.MessageToMessageCodec
+import io.netty.handler.codec.MessageToMessageDecoder
+import io.netty.handler.codec.MessageToMessageEncoder
 
 /**
  * Adds/removes trailing character from messages
@@ -12,6 +14,18 @@ class StringSuffixCodec(val trainlingChar: Char) : MessageToMessageCodec<String,
         out += (msg + trainlingChar)
     }
 
+    override fun decode(ctx: ChannelHandlerContext?, msg: String, out: MutableList<Any>) {
+        out += msg.trimEnd(trainlingChar)
+    }
+}
+
+class StringSuffixEncoder(val trainlingChar: Char) : MessageToMessageEncoder<String>() {
+    override fun encode(ctx: ChannelHandlerContext, msg: String, out: MutableList<Any>) {
+        out += (msg + trainlingChar)
+    }
+}
+
+class StringSuffixDecoder(val trainlingChar: Char) : MessageToMessageDecoder<String>() {
     override fun decode(ctx: ChannelHandlerContext?, msg: String, out: MutableList<Any>) {
         out += msg.trimEnd(trainlingChar)
     }
