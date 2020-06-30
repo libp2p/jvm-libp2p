e82068ecf6eb38613d3a984f88e6338f18678026
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-06-30 17:34:01 +0300

    Bump version to 0.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ import java.nio.file.Paths
 // ./gradlew bintrayUpload -PbintrayUser=<user> -PbintrayApiKey=<api-key>
 
 group = "io.libp2p"
-version = "0.5.1-RELEASE"
+version = "0.5.2-RELEASE"
 description = "a minimal implementation of libp2p for the jvm"
 
 plugins {
