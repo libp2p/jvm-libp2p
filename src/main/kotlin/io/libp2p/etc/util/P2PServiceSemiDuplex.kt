fdec7cb48c94f7da5d37a9deb2a8867d5f9db9e7
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-07-14 15:28:20 +0300

    When closing a stream right upon its creation (closeAbruptly) then we need to ignore that [peerHandler] is null

--- src/main/kotlin/io/libp2p/etc/util/P2PServiceSemiDuplex.kt
+++ src/main/kotlin/io/libp2p/etc/util/P2PServiceSemiDuplex.kt
@@ -36,11 +36,11 @@ abstract class P2PServiceSemiDuplex : P2PService() {
             peerHandler as SDPeerHandler
             when {
                 peerHandler.otherStreamHandler != null -> {
-                    stream.close()
+                    streamHandler.closeAbruptly()
                     throw BadPeerException("Duplicate steam for peer ${peerHandler.peerId}. Closing it silently")
                 }
                 peerHandler.streamHandler.stream.isInitiator == stream.isInitiator -> {
-                    stream.close()
+                    streamHandler.closeAbruptly()
                     throw BadPeerException("Duplicate stream with initiator = ${stream.isInitiator} for peer ${peerHandler.peerId}")
                 }
                 else -> {
