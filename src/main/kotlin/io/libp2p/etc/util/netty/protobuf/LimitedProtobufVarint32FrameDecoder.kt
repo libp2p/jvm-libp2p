4f5aecbfdaac63c69973c51c80e73adb5fb0e836
Meredith Baxter <meredith.baxter@consensys.net>
2020-12-16 13:30:58 -0500

    Add frame length validation

--- src/main/kotlin/io/libp2p/etc/util/netty/protobuf/LimitedProtobufVarint32FrameDecoder.kt
+++ src/main/kotlin/io/libp2p/etc/util/netty/protobuf/LimitedProtobufVarint32FrameDecoder.kt
@@ -5,12 +5,14 @@ import kotlin.Throws
 import io.netty.channel.ChannelHandlerContext
 import io.netty.buffer.ByteBuf
 import io.netty.handler.codec.CorruptedFrameException
+import io.netty.handler.codec.TooLongFrameException
 import java.lang.Exception
 
 /**
  * This class is a modified version of netty's ProtobufVariant32FrameDecoder that enforces limits on the
- * size of the protobuf data.
+ * size of the protobuf data. Limit functionality is based on netty's LengthFieldBasedFrameDecoder.
  * @see [ProtobufVarint32FrameDecoder](https://github.com/netty/netty/blob/e5951d46fc89db507ba7d2968d2ede26378f0b04/codec/src/main/java/io/netty/handler/codec/protobuf/ProtobufVarint32FrameDecoder.java)
+ * @see [LengthFieldBasedFrameDecoder] (https://github.com/netty/netty/blob/8ea0d8f41ac5b0b09229a53690cad7febb4332ef/codec/src/main/java/io/netty/handler/codec/LengthFieldBasedFrameDecoder.java)
  *
  * A decoder that splits the received {@link ByteBuf}s dynamically by the
  * value of the Google Protocol Buffers
@@ -26,23 +28,88 @@ import java.lang.Exception
  *
  * @see CodedInputStream
  */
-internal class LimitedProtobufVarint32FrameDecoder(private val maxFrameLength: Long) : ByteToMessageDecoder() {
+internal class LimitedProtobufVarint32FrameDecoder(private val maxFrameLength: Int) : ByteToMessageDecoder() {
+    private var discardingTooLongFrame = false
+    private var tooLongFrameLength: Int = 0
+    private var bytesToDiscard: Int = 0
 
     @Throws(Exception::class)
     override fun decode(ctx: ChannelHandlerContext, msg: ByteBuf, out: MutableList<Any>) {
+        if (discardingTooLongFrame) {
+            discardFrame(msg)
+        }
+
         msg.markReaderIndex()
         val preIndex = msg.readerIndex()
         val length = readRawVarint32(msg)
         if (preIndex == msg.readerIndex()) {
             return
         }
+
+        // Validate length
         if (length < 0) {
             throw CorruptedFrameException("negative length: $length")
         }
+        if (length > maxFrameLength) {
+            handleFrameExceedingMaxLength(msg, length)
+            return
+        }
+
         if (msg.readableBytes() < length) {
             msg.resetReaderIndex()
         } else {
             out.add(msg.readRetainedSlice(length))
+        }
+    }
+
+    private fun handleFrameExceedingMaxLength(msg: ByteBuf, frameLength: Int) {
+        val discard = frameLength - msg.readableBytes()
+        tooLongFrameLength = frameLength
+        if (discard < 0) {
+            // buffer contains more bytes then the frameLength so we can discard all now
+            msg.skipBytes(frameLength)
+        } else {
+            // Enter the discard mode and discard everything received so far.
+            discardingTooLongFrame = true
+            bytesToDiscard = discard
+            msg.skipBytes(msg.readableBytes())
+        }
+        failIfNecessary(true)
+    }
+
+    private fun discardFrame(msg: ByteBuf) {
+        var bytesToDiscard = bytesToDiscard
+        val localBytesToDiscard = Math.min(bytesToDiscard, msg.readableBytes())
+        msg.skipBytes(localBytesToDiscard)
+        bytesToDiscard -= localBytesToDiscard
+        this.bytesToDiscard = bytesToDiscard
+        failIfNecessary(false)
+    }
+
+    private fun failIfNecessary(firstDetectionOfTooLongFrame: Boolean) {
+        val tooLongFrameLength = tooLongFrameLength
+        if (bytesToDiscard == 0) {
+            // Reset to the initial state.
+            this.tooLongFrameLength = 0
+            discardingTooLongFrame = false
+        }
+        // Tell the handlers that the frame was too large.
+        if (firstDetectionOfTooLongFrame) {
+            fail(tooLongFrameLength)
+        }
+    }
+
+    private fun fail(frameLength: Int) {
+        if (frameLength > 0) {
+            throw TooLongFrameException(
+                "Adjusted frame length exceeds " + maxFrameLength +
+                        ": " + frameLength + " - discarded"
+            )
+        } else {
+            throw TooLongFrameException(
+                ("Adjusted frame length exceeds " + maxFrameLength +
+                        " - discarding")
+            )
         }
     }
 
