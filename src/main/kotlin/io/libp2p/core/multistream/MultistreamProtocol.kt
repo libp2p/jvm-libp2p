c8a625f1f8bd6005c9c332f9992d5d83fde22dd6
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-12-25 17:46:38 +0300

    Add Multistream pre/postHandler

--- src/main/kotlin/io/libp2p/core/multistream/MultistreamProtocol.kt
+++ src/main/kotlin/io/libp2p/core/multistream/MultistreamProtocol.kt
@@ -1,18 +1,9 @@
 package io.libp2p.core.multistream
 
+import io.libp2p.core.P2PChannelHandler
 import io.libp2p.multistream.MultistreamImpl
 
-val MultistreamProtocol_v_1_0_0: MultistreamProtocol = object : MultistreamProtocol {
-
-    override val version = "1.0.0"
-
-    /**
-     * Creates [Multistream] implementation with a list of protocol bindings
-     */
-    override fun <TController> create(
-        bindings: List<ProtocolBinding<TController>>
-    ): Multistream<TController> = MultistreamImpl(bindings)
-}
+val MultistreamProtocol_v_1_0_0: MultistreamProtocol = MultistreamProtocolDebug_v_1_0_0()
 
 interface MultistreamProtocol {
 
@@ -22,4 +13,28 @@ interface MultistreamProtocol {
      * Creates [Multistream] implementation with a list of protocol bindings
      */
     fun <TController> create(bindings: List<ProtocolBinding<TController>>): Multistream<TController>
+}
+
+interface MultistreamProtocolDebug : MultistreamProtocol {
+
+    fun copyWithHandlers(
+        preHandler: P2PChannelHandler<*>? = null,
+        postHandler: P2PChannelHandler<*>? = null
+    ): MultistreamProtocol
+}
+
+class MultistreamProtocolDebug_v_1_0_0(
+    private val preHandler: P2PChannelHandler<*>? = null,
+    private val postHandler: P2PChannelHandler<*>? = null
+) : MultistreamProtocolDebug {
+
+    override val version = "1.0.0"
+
+    override fun <TController> create(bindings: List<ProtocolBinding<TController>>) =
+        MultistreamImpl(bindings, preHandler, postHandler)
+
+    override fun copyWithHandlers(
+        preHandler: P2PChannelHandler<*>?,
+        postHandler: P2PChannelHandler<*>?
+    ) = MultistreamProtocolDebug_v_1_0_0(preHandler, postHandler)
 }
--- src/main/kotlin/io/libp2p/multistream/MultistreamImpl.kt
+++ src/main/kotlin/io/libp2p/multistream/MultistreamImpl.kt
@@ -1,19 +1,26 @@
 package io.libp2p.multistream
 
 import io.libp2p.core.P2PChannel
+import io.libp2p.core.P2PChannelHandler
 import io.libp2p.core.multistream.Multistream
 import io.libp2p.core.multistream.ProtocolBinding
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CopyOnWriteArrayList
 
-class MultistreamImpl<TController>(initList: List<ProtocolBinding<TController>> = listOf()) :
-    Multistream<TController> {
+class MultistreamImpl<TController>(
+    initList: List<ProtocolBinding<TController>> = listOf(),
+    val preHandler: P2PChannelHandler<*>? = null,
+    val postHandler: P2PChannelHandler<*>? = null
+) : Multistream<TController> {
 
     override val bindings: MutableList<ProtocolBinding<TController>> =
         CopyOnWriteArrayList(initList)
 
     override fun initChannel(ch: P2PChannel): CompletableFuture<TController> {
         return with(ch) {
+            preHandler?.also {
+                it.initChannel(ch)
+            }
             pushHandler(
                 if (ch.isInitiator) {
                     Negotiator.createRequesterInitializer(*bindings.flatMap { it.protocolDescriptor.announceProtocols }.toTypedArray())
@@ -21,6 +28,9 @@ class MultistreamImpl<TController>(initList: List<ProtocolBinding<TController>> 
                     Negotiator.createResponderInitializer(bindings.map { it.protocolDescriptor.protocolMatcher })
                 }
             )
+            postHandler?.also {
+                it.initChannel(ch)
+            }
             val protocolSelect = ProtocolSelect(bindings)
             pushHandler(protocolSelect)
             protocolSelect.selectedFuture
