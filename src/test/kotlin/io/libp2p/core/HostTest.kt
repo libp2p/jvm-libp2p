2483c8292957aeff4039b4b20585e8fbc764bd02
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2020-12-28 18:11:52 +0300

    Add protocol interceptor test

--- src/test/kotlin/io/libp2p/core/HostTest.kt
+++ src/test/kotlin/io/libp2p/core/HostTest.kt
@@ -1,10 +1,14 @@
 package io.libp2p.core
 
+import io.libp2p.core.multistream.ProtocolMatcher
+import io.libp2p.etc.PROTOCOL
+import io.libp2p.etc.types.seconds
 import io.libp2p.etc.types.toByteArray
 import io.libp2p.mux.MuxFrame
 import io.libp2p.protocol.Ping
 import io.libp2p.protocol.PingBinding
 import io.libp2p.protocol.PingProtocol
+import io.libp2p.tools.Echo
 import io.libp2p.tools.HostFactory
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.Unpooled
@@ -60,6 +64,52 @@ class HostTest {
 
         assertThat(streamVisitor1.inboundData.size).isGreaterThan(packetsCount1)
         assertThat(streamVisitor2.inboundData.size).isGreaterThan(packetsCount2)
+    }
+
+    @Test
+    fun `test protocol stream interceptor`() {
+        val host1 = hostFactory.createHost()
+
+        class TestInterceptor : ProtocolInterceptor(ProtocolMatcher.prefix("/test/echo")) {
+            var interceptRead = false
+            var interceptWrite = false
+            override fun interceptRead(buf: ByteBuf) =
+                if (interceptRead)
+                    Unpooled.wrappedBuffer("RRR".toByteArray(Charsets.UTF_8))
+                else buf
+            override fun interceptWrite(buf: ByteBuf) =
+                if (interceptWrite)
+                    Unpooled.wrappedBuffer("WWW".toByteArray(Charsets.UTF_8))
+                else buf
+        }
+        val interceptor = TestInterceptor()
+
+        hostFactory.hostBuilderModifier = {
+            debug {
+                streamHandler.addHandler(interceptor)
+            }
+        }
+        val host2 = hostFactory.createHost()
+
+        val ping = Ping().dial(host1.host, host2.peerId, host2.listenAddress)
+        val pingCtrl = ping.controller.get(5, TimeUnit.SECONDS)
+        val echo = Echo().dial(host1.host, host2.peerId, host2.listenAddress)
+        val echoCtrl = echo.controller.get(5, TimeUnit.SECONDS)
+
+        assertThat(pingCtrl.ping()).succeedsWithin(5.seconds)
+        assertThat(echoCtrl.echo("AAA")).succeedsWithin(5.seconds).isEqualTo("AAA")
+
+        interceptor.interceptRead = true
+
+        // interceptor doesn't affect /ping
+        assertThat(pingCtrl.ping()).succeedsWithin(5.seconds)
+        // ... but modifies /echo
+        assertThat(echoCtrl.echo("AAA")).succeedsWithin(5.seconds).isEqualTo("RRR")
+
+        interceptor.interceptRead = false
+        interceptor.interceptWrite = true
+
+        assertThat(echoCtrl.echo("AAA")).succeedsWithin(5.seconds).isEqualTo("WWW")
     }
 
     @Test
@@ -205,5 +255,62 @@ class HostTest {
 
     class TestByteBufChannelHandler<TChannel : P2PChannel>(id: String) : TestChannelHandler<TChannel, ByteBuf>(id) {
         override fun retainMessage(msg: ByteBuf) = msg.retainedSlice()
+    }
+
+    abstract class ProtocolInterceptor(val protocol: ProtocolMatcher) : ChannelVisitor<Stream> {
+        abstract fun interceptRead(buf: ByteBuf): ByteBuf
+        abstract fun interceptWrite(buf: ByteBuf): ByteBuf
+
+        override fun visit(stream: Stream) {
+            var matched = false
+            stream.pushHandler(object : ChannelDuplexHandler() {
+
+                override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
+                    if (match(ctx)) {
+                        msg as ByteBuf
+                        println("####   --> : $msg")
+                        val modifiedRead = interceptRead(msg)
+                        ctx.fireChannelRead(modifiedRead)
+                    } else {
+                        ctx.fireChannelRead(msg)
+                    }
+                }
+
+                override fun write(ctx: ChannelHandlerContext, msg: Any, promise: ChannelPromise) {
+                    if (match(ctx)) {
+                        msg as ByteBuf
+                        println("#### <--   : $msg")
+                        val modifiedWrite = interceptWrite(msg)
+                        ctx.write(modifiedWrite, promise)
+                    } else {
+                        ctx.write(msg, promise)
+                    }
+                }
+
+                fun match(ctx: ChannelHandlerContext): Boolean {
+                    if (!matched) {
+                        matched = true
+                        val protoFut = ctx.channel().attr(PROTOCOL).get()
+                        assert(protoFut.isDone)
+                        val proto = protoFut.get()
+                        if (!protocol.matches(proto)) {
+                            ctx.pipeline().remove(this)
+                            println("### protocol didn't match: $proto")
+                            return false
+                        } else {
+                            println("### protocol matched: $proto")
+                            return true
+                        }
+                    } else {
+                        return true
+                    }
+                }
+
+                override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
+                    cause.printStackTrace()
+                    ctx.fireExceptionCaught(cause)
+                }
+            })
+        }
     }
 }
