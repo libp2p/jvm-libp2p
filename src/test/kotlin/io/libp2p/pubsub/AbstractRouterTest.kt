ced26a7c6f6f4bf50ebd1e6368ba1503dae81333
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2022-02-02 13:13:13 +0300

    Add more tests

--- src/test/kotlin/io/libp2p/pubsub/AbstractRouterTest.kt
+++ src/test/kotlin/io/libp2p/pubsub/AbstractRouterTest.kt
@@ -1,5 +1,6 @@
 package io.libp2p.pubsub
 
+import io.libp2p.etc.types.toProtobuf
 import io.libp2p.pubsub.TopicSubscriptionFilter.AllowAllTopicSubscriptionFilter
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -8,7 +9,7 @@ import java.util.concurrent.CompletableFuture
 
 class AbstractRouterTest {
 
-    class TestRouter(val msgValidator: (Rpc.RPCOrBuilder) -> Boolean) :
+    private class TestRouter(val msgValidator: (Rpc.RPCOrBuilder) -> Boolean) :
         AbstractRouter(AllowAllTopicSubscriptionFilter()) {
         override val protocol = PubsubProtocol.Floodsub
         override fun broadcastOutbound(msg: PubsubMessage): CompletableFuture<Unit> =
@@ -21,7 +22,8 @@ class AbstractRouterTest {
         fun testMerge(parts: List<Rpc.RPC>): List<Rpc.RPC> = mergeMessageParts(parts)
     }
 
-    fun Collection<Rpc.RPC>.merge() = this.fold(Rpc.RPC.newBuilder()) { bld, part -> bld.mergeFrom(part) }.build()
+    private fun Collection<Rpc.RPC>.merge(): Rpc.RPC =
+        this.fold(Rpc.RPC.newBuilder()) { bld, part -> bld.mergeFrom(part) }.build()
 
     @Test
     fun `test many subscriptions split to several messages`() {
@@ -57,6 +59,29 @@ class AbstractRouterTest {
         val msgs = router.testMerge(parts)
 
         assertThat(msgs).hasSize(1)
+        assertThat(msgs.merge()).isEqualTo(parts.merge())
+    }
+
+    @Test
+    fun `test that split doesn't result in topic publish before subscribe`() {
+        val router = TestRouter { it.subscriptionsCount <= 5 }
+        val parts = (0 until 6).map {
+            Rpc.RPC.newBuilder().addSubscriptions(
+                Rpc.RPC.SubOpts.newBuilder()
+                    .setTopicid("topic-$it")
+                    .setSubscribe(true)
+                    .build()
+            ).build()
+        } + Rpc.RPC.newBuilder().addPublish(
+            Rpc.Message.newBuilder()
+                .addTopicIDs("topic-5")
+                .setData(byteArrayOf(11).toProtobuf())
+        ).build()
+        val msgs = router.testMerge(parts)
+
+        assertThat(msgs).hasSize(2)
+        assertThat(msgs[0].publishCount).isZero()
+        assertThat(msgs[1].publishCount).isEqualTo(1)
         assertThat(msgs.merge()).isEqualTo(parts.merge())
     }
 }
