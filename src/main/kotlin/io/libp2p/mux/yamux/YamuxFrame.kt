21273fd9f805fbac3db0db1c0b6dc9129cb30854
ian <ianopolous@protonmail.com>
2023-02-10 14:43:39 +0000

    First attempt at yamux implementation

---
+++ src/main/kotlin/io/libp2p/mux/yamux/YamuxFrame.kt
@@ -0,0 +1,22 @@
+package io.libp2p.mux.yamux
+
+import io.libp2p.etc.types.toByteArray
+import io.libp2p.etc.types.toHex
+import io.libp2p.etc.util.netty.mux.MuxId
+import io.netty.buffer.ByteBuf
+import io.netty.buffer.DefaultByteBufHolder
+import io.netty.buffer.Unpooled
+
+/**
+ * Contains the fields that comprise a yamux frame.
+ * @param streamId the ID of the stream.
+ * @param flag the flag value for this frame.
+ * @param data the data segment.
+ */
+class YamuxFrame(val id: MuxId, val type: Int, val flags: Int, val lenData: Int, val data: ByteBuf? = null) :
+    DefaultByteBufHolder(data ?: Unpooled.EMPTY_BUFFER) {
+
+    override fun toString(): String {
+        return "YamuxFrame(id=$id, type=$type, flag=$flags, data=${data?.toByteArray()?.toHex()})"
+    }
+}
