163d2606bb761b9d85d07a2e547fb5690715018d
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2023-02-21 11:50:45 +0400

    Refactor Simulation2 to GossipScoreTestSimulation

--- tools/simulator/src/main/kotlin/io/libp2p/simulate/main/GossipScoreTestSimulation.kt
+++ tools/simulator/src/main/kotlin/io/libp2p/simulate/main/GossipScoreTestSimulation.kt
@@ -2,24 +2,19 @@ package io.libp2p.simulate.main
 
 import io.libp2p.core.pubsub.Topic
 import io.libp2p.etc.types.seconds
-import io.libp2p.pubsub.gossip.GossipTopicScoreParams
-import io.libp2p.pubsub.gossip.GossipTopicsScoreParams
-import io.libp2p.pubsub.gossip.builders.GossipRouterBuilder
-import io.libp2p.pubsub.gossip.builders.GossipScoreParamsBuilder
-import io.libp2p.pubsub.gossip.builders.GossipTopicScoreParamsBuilder
 import io.libp2p.simulate.gossip.*
 import io.libp2p.simulate.gossip.router.SimGossipRouterBuilder
 import io.libp2p.simulate.stats.StatsFactory
 import io.libp2p.simulate.topology.RandomNPeers
 import io.libp2p.simulate.util.millis
-import io.libp2p.simulate.util.propertiesAsMap
-import org.junit.jupiter.api.Test
-import java.time.Duration
 
-class Simulation2 {
+fun main() {
+    GossipScoreTestSimulation().run()
+}
 
-    @Test
-    fun sim() {
+class GossipScoreTestSimulation {
+
+    fun run() {
 
         val simConfig = GossipSimConfig(
             totalPeers = 1000,
@@ -37,17 +32,6 @@ class Simulation2 {
             }
         }
 
-        // prints interchange of 2 peers
-//        val peer_819 = PeerId.fromBase58("QmerDZ9nE6GJmXSbH3hYzptR1uae7JS2ZphU4kNwGEeDNg")
-//        val peer_90 = PeerId.fromBase58("QmcWmvf2sRyh2DSHmEsu83GGxzQMn661uWdxJLcrvRzJ3H")
-//        val simPeerModifier = { num :Int, peer: GossipSimPeer ->
-//            if (num == 90) {
-//                peer.pubsubLogs = { it == peer_819 }
-//            }
-//            if (num == 819) {
-//                peer.pubsubLogs = { it == peer_90 }
-//            }
-//        }
         val simPeerModifier = { _: Int, _: GossipSimPeer -> }
 
         val simNetwork = GossipSimNetwork(simConfig, gossipRouterCtor, simPeerModifier)
@@ -89,21 +73,12 @@ class Simulation2 {
             it += results.entries.map { it.value.size.toDouble() / (simConfig.totalPeers - 1) }
         }
 
-//        val connections = simNetwork.peers.values.flatMap { peer ->
-//            peer.getConnectedPeers().map { peer to it as GossipSimPeer }
-//        }
-//        val scoredConnections = connections.map { (from, to) ->
-//            val gossipFrom = from.router as GossipRouter
-//            val toPeerHandler = gossipFrom.peers.filter { it.peerId == to.peerId }.first()
-//            Triple(gossipFrom.score.score(toPeerHandler), from, to)
-//        }.sortedBy { it.first }
-
-        println(msgDelayStats)
-        println(msgDeliveryStats)
-        println(getScoreStats(simNetwork))
+        println("Message delivery delay stats: $msgDelayStats")
+        println("Ratio of messages delivered: $msgDeliveryStats")
+        println("Gossip score stats: " + getScoreStats(simNetwork))
     }
 
-    fun getScoreStats(network: GossipSimNetwork) =
+    private fun getScoreStats(network: GossipSimNetwork) =
         StatsFactory.DEFAULT.createStats("gossipScore").also {
             it += network.peers.values
                 .map { it.router }
@@ -113,29 +88,4 @@ class Simulation2 {
                     }
                 }
         }
-
-    fun GossipTopicsScoreParams.withTopic(topic: String, params: GossipTopicScoreParams) =
-        GossipTopicsScoreParams(defaultParams, topicParams + mapOf(topic to params))
-
-    @Test
-    fun a() {
-        fun iterateMeshMessageDeliveryWindow(values: List<Duration>) =
-            values.map {
-                GossipTopicScoreParamsBuilder(Eth2DefaultBlockTopicParams)
-                    .meshMessageDeliveryWindow(it)
-                    .build()
-            }.map {
-                Eth2DefaultTopicsParams.withTopic(BlocksTopic, it)
-            }.map {
-                GossipScoreParamsBuilder(Eth2DefaultScoreParams)
-                    .peerScoreParams(Eth2DefaultPeerScoreParams)
-                    .topicsScoreParams(it)
-                    .build()
-            }
-
-        val param =
-            iterateMeshMessageDeliveryWindow(listOf(20, 50, 100, 200, 500, 1000).map { it.millis })
-        val map = param[0].propertiesAsMap()
-        println(map)
-    }
 }
