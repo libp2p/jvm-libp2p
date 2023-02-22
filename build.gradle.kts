de50af7549048bc2d26fc321d8f8c9b7ac7ab82e
ian <ianopolous@protonmail.com>
2023-02-22 10:16:20 +0000

    Update to Java 17 minimum, which Ed25519 asn1 needs

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,12 +80,12 @@ protobuf {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjvm-default=all")
     }
@@ -129,7 +129,7 @@ tasks.dokkaHtml.configure {
     outputDirectory.set(buildDir.resolve("dokka"))
     dokkaSourceSets {
         configureEach {
-            jdkVersion.set(11)
+            jdkVersion.set(17)
             reportUndocumented.set(false)
             externalDocumentationLink {
                 url.set(URL("https://netty.io/4.1/api/"))
