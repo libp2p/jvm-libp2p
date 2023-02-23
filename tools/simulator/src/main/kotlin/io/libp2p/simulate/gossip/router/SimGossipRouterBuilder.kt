e31abf92203f82ce3f0cf42a428a4eed5b22590c
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2023-02-23 20:13:18 +0400

    Custom SimPubsubMessage with content based messageId

--- tools/simulator/src/main/kotlin/io/libp2p/simulate/gossip/router/SimGossipRouterBuilder.kt
+++ tools/simulator/src/main/kotlin/io/libp2p/simulate/gossip/router/SimGossipRouterBuilder.kt
@@ -1,12 +1,21 @@
 package io.libp2p.simulate.gossip.router
 
+import io.libp2p.core.crypto.sha256
+import io.libp2p.etc.types.toWBytes
+import io.libp2p.pubsub.AbstractPubsubMessage
+import io.libp2p.pubsub.MessageId
 import io.libp2p.pubsub.gossip.GossipRouter
 import io.libp2p.pubsub.gossip.builders.GossipRouterBuilder
+import pubsub.pb.Rpc
 import kotlin.time.Duration
 
 class SimGossipRouterBuilder : GossipRouterBuilder() {
     var serializeMessagesToBytes: Boolean = false
     var additionalHeartbeatDelay: Duration = Duration.ZERO
+
+    init {
+        messageFactory = { SimPubsubMessage(it) }
+    }
 
     override fun createGossipRouter(): GossipRouter {
         val gossipScore =
@@ -33,4 +42,10 @@ class SimGossipRouterBuilder : GossipRouterBuilder() {
         router.eventBroadcaster.listeners += gossipRouterEventListeners
         return router
     }
+
+    class SimPubsubMessage(override val protobufMessage: Rpc.Message) : AbstractPubsubMessage() {
+        override val messageId: MessageId =
+            sha256(protobufMessage.data.toByteArray()).sliceArray(0..7).toWBytes()
+    }
+
 }
