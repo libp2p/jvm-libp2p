edbe45799391239023988b3c64f78ec3349beedb
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2023-05-11 21:43:35 +0400

    Exclude common gradle configuration by property rather than project name

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,52 +26,52 @@ plugins {
     id("com.android.application") version "7.4.2" apply false
 }
 
-allprojects {
+fun Project.getBooleanPropertyOrFalse(propName: String) =
+    (this.properties[propName] as? String)?.toBoolean() ?: false
+
+configure(
+    allprojects
+        .filterNot {
+            it.getBooleanPropertyOrFalse("libp2p.gradle.custom")
+        }
+) {
     group = "io.libp2p"
     version = "develop"
 
-    repositories {
-        mavenCentral()
-        maven("https://artifacts.consensys.net/public/maven/maven/")
-        google()
-    }
+    apply(plugin = "kotlin")
+    apply(plugin = "idea")
+    apply(plugin = "java")
 
-    val isAndroid = name.contains("android", ignoreCase = true)
-    if (!isAndroid) {
-        apply(plugin = "kotlin")
-        apply(plugin = "idea")
-        apply(plugin = "java")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    apply(plugin = "maven-publish")
+    apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "org.jmailen.kotlinter")
+    apply(plugin = "java-test-fixtures")
+    apply(plugin = "io.spring.dependency-management")
+    apply(from = "$rootDir/versions.gradle")
 
-        apply(plugin = "io.gitlab.arturbosch.detekt")
-        apply(plugin = "maven-publish")
-        apply(plugin = "org.jetbrains.dokka")
-        apply(plugin = "org.jmailen.kotlinter")
-        apply(plugin = "java-test-fixtures")
-        apply(plugin = "io.spring.dependency-management")
-        apply(from = "$rootDir/versions.gradle")
-
-        dependencies {
+    dependencies {
 
             implementation(kotlin("stdlib-jdk8"))
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
-            implementation("com.google.guava:guava")
-            implementation("org.apache.logging.log4j:log4j-api")
+        implementation("com.google.guava:guava")
+        implementation("org.apache.logging.log4j:log4j-api")
 
-            testFixturesImplementation("org.apache.logging.log4j:log4j-api")
-            testFixturesImplementation("com.google.guava:guava")
+        testFixturesImplementation("org.apache.logging.log4j:log4j-api")
+        testFixturesImplementation("com.google.guava:guava")
 
-            testImplementation("org.junit.jupiter:junit-jupiter")
-            testImplementation("org.junit.jupiter:junit-jupiter-params")
-            testImplementation("io.mockk:mockk")
-            testImplementation("org.assertj:assertj-core")
-            testImplementation("org.apache.logging.log4j:log4j-core")
-        }
+        testImplementation("org.junit.jupiter:junit-jupiter")
+        testImplementation("org.junit.jupiter:junit-jupiter-params")
+        testImplementation("io.mockk:mockk")
+        testImplementation("org.assertj:assertj-core")
+        testImplementation("org.apache.logging.log4j:log4j-core")
+    }
 
-        java {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
-        }
+    java {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 
     tasks.withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "11"
@@ -86,85 +86,84 @@ allprojects {
     }
 
 // Parallel build execution
-        tasks.test {
-            description = "Runs the unit tests."
+    tasks.test {
+        description = "Runs the unit tests."
 
-            useJUnitPlatform {
-                excludeTags("interop")
-            }
+        useJUnitPlatform {
+            excludeTags("interop")
+        }
 
-            testLogging {
-                events("FAILED")
-                exceptionFormat = TestExceptionFormat.FULL
-                showCauses = true
-                showExceptions = true
-                showStackTraces = true
-            }
+        testLogging {
+            events("FAILED")
+            exceptionFormat = TestExceptionFormat.FULL
+            showCauses = true
+            showExceptions = true
+            showStackTraces = true
+        }
 
-            // disabling the parallel test runs for the time being due to port collisions
-            // If GRADLE_MAX_TEST_FORKS is not set, use half the available processors
+        // disabling the parallel test runs for the time being due to port collisions
+        // If GRADLE_MAX_TEST_FORKS is not set, use half the available processors
 //    maxParallelForks = (System.getenv("GRADLE_MAX_TEST_FORKS")?.toInt() ?:
 //    Runtime.getRuntime().availableProcessors().div(2))
-        }
-
-        kotlinter {
-            disabledRules = arrayOf("no-wildcard-imports", "enum-entry-name-case")
-        }
-
-        val sourcesJar by tasks.registering(Jar::class) {
-            archiveClassifier.set("sources")
-            from(sourceSets.main.get().allSource)
-        }
-
-        tasks.dokkaHtml.configure {
-            outputDirectory.set(buildDir.resolve("dokka"))
-            dokkaSourceSets {
-                configureEach {
-                    jdkVersion.set(11)
-                    reportUndocumented.set(false)
-                    externalDocumentationLink {
-                        url.set(URL("https://netty.io/4.1/api/"))
-                    }
-                }
-            }
-        }
-
-        val dokkaJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
-            group = JavaBasePlugin.DOCUMENTATION_GROUP
-            val dokkaJavadocTask = tasks.getByName("dokkaJavadoc")
-            dependsOn(dokkaJavadocTask)
-            archiveClassifier.set("javadoc")
-            from(dokkaJavadocTask.outputs)
-        }
-
-        publishing {
-            repositories {
-                maven {
-                    name = "cloudsmith"
-                    url = uri("https://api-g.cloudsmith.io/maven/libp2p/jvm-libp2p")
-                    credentials {
-                        username = findProperty("cloudsmithUser") as String?
-                        password = findProperty("cloudsmithApiKey") as String?
-                    }
-                }
-            }
-            if (hasProperty("mavenArtifactId")) {
-                publications {
-                    register("mavenJava", MavenPublication::class) {
-                        from(components["java"])
-                        artifact(sourcesJar.get())
-                        artifact(dokkaJar.get())
-                        groupId = "io.libp2p"
-                        artifactId = project.property("mavenArtifactId") as String
-                    }
-                }
-            }
-        }
-
-        detekt {
-            config = files("$rootDir/detekt/config.yml")
-            buildUponDefaultConfig = true
-        }
-
     }
+
+    kotlinter {
+        disabledRules = arrayOf("no-wildcard-imports", "enum-entry-name-case")
+    }
+
+    val sourcesJar by tasks.registering(Jar::class) {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    tasks.dokkaHtml.configure {
+        outputDirectory.set(buildDir.resolve("dokka"))
+        dokkaSourceSets {
+            configureEach {
+                jdkVersion.set(11)
+                reportUndocumented.set(false)
+                externalDocumentationLink {
+                    url.set(URL("https://netty.io/4.1/api/"))
+                }
+            }
+        }
+    }
+
+    val dokkaJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+        group = JavaBasePlugin.DOCUMENTATION_GROUP
+        val dokkaJavadocTask = tasks.getByName("dokkaJavadoc")
+        dependsOn(dokkaJavadocTask)
+        archiveClassifier.set("javadoc")
+        from(dokkaJavadocTask.outputs)
+    }
+
+    publishing {
+        repositories {
+            maven {
+                name = "cloudsmith"
+                url = uri("https://api-g.cloudsmith.io/maven/libp2p/jvm-libp2p")
+                credentials {
+                    username = findProperty("cloudsmithUser") as String?
+                    password = findProperty("cloudsmithApiKey") as String?
+                }
+            }
+        }
+        if (hasProperty("mavenArtifactId")) {
+            publications {
+                register("mavenJava", MavenPublication::class) {
+                    from(components["java"])
+                    artifact(sourcesJar.get())
+                    artifact(dokkaJar.get())
+                    groupId = "io.libp2p"
+                    artifactId = project.property("mavenArtifactId") as String
+                }
+            }
+        }
+    }
+
+    detekt {
+        config = files("$rootDir/detekt/config.yml")
+        buildUponDefaultConfig = true
+    }
+
 }
