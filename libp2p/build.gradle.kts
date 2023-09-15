31a7e7fb5ca99f4cf19ef78010c0a3543ba1df6d
Stefan Bratanov <stefan.bratanov93@gmail.com>
2023-09-15 09:45:35 +0100

    Dependencies updates

--- libp2p/build.gradle.kts
+++ libp2p/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.google.protobuf").version("0.9.2")
-    id("me.champeau.jmh").version("0.6.8")
+    id("com.google.protobuf").version("0.9.4")
+    id("me.champeau.jmh").version("0.7.1")
 }
 
 // https://docs.gradle.org/current/userguide/java_testing.html#ex-disable-publishing-of-test-fixtures-variants
@@ -17,11 +17,12 @@ dependencies {
 
     api("com.google.protobuf:protobuf-java")
 
+    implementation("com.github.multiformats:java-multibase")
     implementation("tech.pegasys:noise-java")
 
-    implementation("org.bouncycastle:bcprov-jdk15on")
-    implementation("org.bouncycastle:bcpkix-jdk15on")
-    implementation("org.bouncycastle:bctls-jdk15on")
+    implementation("org.bouncycastle:bcprov-jdk18on")
+    implementation("org.bouncycastle:bcpkix-jdk18on")
+    implementation("org.bouncycastle:bctls-jdk18on")
 
     testImplementation(project(":tools:schedulers"))
 
