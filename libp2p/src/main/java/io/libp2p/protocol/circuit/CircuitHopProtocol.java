a4c2cc7833fa29bad2f2bb5402d7e4d3e181381c
ian <ianopolous@protonmail.com>
2023-12-02 23:42:03 +0000

    Initial import of circuit relay

---
+++ libp2p/src/main/java/io/libp2p/protocol/circuit/CircuitHopProtocol.java
@@ -0,0 +1,349 @@
+package io.libp2p.protocol.circuit;
+
+import com.google.protobuf.*;
+import io.libp2p.core.*;
+import io.libp2p.core.Stream;
+import io.libp2p.core.crypto.*;
+import io.libp2p.core.multiformats.*;
+import io.libp2p.core.multistream.*;
+import io.libp2p.etc.util.netty.*;
+import io.libp2p.protocol.*;
+import io.netty.buffer.*;
+import io.netty.channel.*;
+import org.jetbrains.annotations.*;
+import io.libp2p.protocol.circuit.pb.*;
+import io.libp2p.protocol.circuit.crypto.pb.*;
+
+import java.io.*;
+import java.nio.charset.*;
+import java.time.*;
+import java.time.Duration;
+import java.time.temporal.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class CircuitHopProtocol extends ProtobufProtocolHandler<CircuitHopProtocol.HopController> {
+
+    public static class Binding extends StrictProtocolBinding<HopController> implements HostConsumer {
+        private final CircuitHopProtocol hop;
+
+        private Binding(CircuitHopProtocol hop) {
+            super("/libp2p/circuit/relay/0.2.0/hop", hop);
+            this.hop = hop;
+        }
+
+        public Binding(RelayManager manager, CircuitStopProtocol.Binding stop) {
+            this(new CircuitHopProtocol(manager, stop));
+        }
+
+        @Override
+        public void setHost(Host us) {
+            hop.setHost(us);
+        }
+    }
+
+    private static void putUvarint(OutputStream out, long x) throws IOException {
+        while (x >= 0x80) {
+            out.write((byte)(x | 0x80));
+            x >>= 7;
+        }
+        out.write((byte)x);
+    }
+    public static byte[] createVoucher(PrivKey priv,
+                                       PeerId relay,
+                                       PeerId requestor,
+                                       LocalDateTime expiry) {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        try {
+            putUvarint(bout, 0x0302);
+        } catch (IOException e) {}
+        byte[] typeMulticodec = bout.toByteArray();
+        byte[] payload = VoucherOuterClass.Voucher.newBuilder()
+                .setRelay(ByteString.copyFrom(relay.getBytes()))
+                .setPeer(ByteString.copyFrom(requestor.getBytes()))
+                .setExpiration(expiry.toEpochSecond(ZoneOffset.UTC) * 1_000_000_000)
+                .build().toByteArray();
+        byte[] signDomain = "libp2p-relay-rsvp".getBytes(StandardCharsets.UTF_8);
+        ByteArrayOutputStream toSign = new ByteArrayOutputStream();
+        try {
+            putUvarint(toSign, signDomain.length);
+            toSign.write(signDomain);
+            putUvarint(toSign, typeMulticodec.length);
+            toSign.write(typeMulticodec);
+            putUvarint(toSign, payload.length);
+            toSign.write(payload);
+        } catch (IOException e) {}
+        byte[] signature = priv.sign(toSign.toByteArray());
+        return Crypto.Envelope.newBuilder()
+                .setPayloadType(ByteString.copyFrom(typeMulticodec))
+                .setPayload(ByteString.copyFrom(payload))
+                .setPublicKey(Crypto.PublicKey.newBuilder()
+                        .setTypeValue(priv.publicKey().getKeyType().getNumber())
+                        .setData(ByteString.copyFrom(priv.publicKey().raw())))
+                .setSignature(ByteString.copyFrom(signature))
+                .build().toByteArray();
+    }
+
+    public static class Reservation {
+        public final LocalDateTime expiry;
+        public final int durationSeconds;
+        public final long maxBytes;
+        public final byte[] voucher;
+
+        public Reservation(LocalDateTime expiry, int durationSeconds, long maxBytes, byte[] voucher) {
+            this.expiry = expiry;
+            this.durationSeconds = durationSeconds;
+            this.maxBytes = maxBytes;
+            this.voucher = voucher;
+        }
+    }
+
+    public interface RelayManager {
+        boolean hasReservation(PeerId source);
+
+        Optional<Reservation> createReservation(PeerId requestor);
+
+        Optional<Reservation> allowConnection(PeerId target, PeerId initiator);
+
+        static RelayManager limitTo(PrivKey priv, PeerId relayPeerId, int concurrent) {
+            return new RelayManager() {
+                Map<PeerId, Reservation> reservations = new HashMap<>();
+
+                @Override
+                public synchronized boolean hasReservation(PeerId source) {
+                    return reservations.containsKey(source);
+                }
+
+                @Override
+                public synchronized Optional<Reservation> createReservation(PeerId requestor) {
+                    if (reservations.size() >= concurrent)
+                        return Optional.empty();
+                    LocalDateTime now = LocalDateTime.now();
+                    LocalDateTime expiry = now.plusHours(1);
+                    byte[] voucher = createVoucher(priv, relayPeerId, requestor, now);
+                    Reservation resv = new Reservation(expiry, 120, 4096, voucher);
+                    reservations.put(requestor, resv);
+                    return Optional.of(resv);
+                }
+
+                @Override
+                public synchronized Optional<Reservation> allowConnection(PeerId target, PeerId initiator) {
+                    return Optional.ofNullable(reservations.get(target));
+                }
+            };
+        }
+    }
+
+    public interface HopController {
+        CompletableFuture<Circuit.HopMessage> rpc(Circuit.HopMessage req);
+
+        default CompletableFuture<Reservation> reserve() {
+            return rpc(Circuit.HopMessage.newBuilder()
+                    .setType(Circuit.HopMessage.Type.RESERVE)
+                    .build())
+                    .thenApply(msg -> {
+                        if (msg.getStatus() == Circuit.Status.OK) {
+                            long expiry = msg.getReservation().getExpire();
+                            return new Reservation(LocalDateTime.ofEpochSecond(expiry, 0, ZoneOffset.UTC),
+                                    msg.getLimit().getDuration(), msg.getLimit().getData(), msg.getReservation().getVoucher().toByteArray());
+                        }
+                        throw new IllegalStateException(msg.getStatus().name());
+                    });
+        }
+
+        CompletableFuture<Stream> connect(PeerId target);
+    }
+
+    public static class Sender implements ProtocolMessageHandler<Circuit.HopMessage>, HopController {
+        private final Stream stream;
+        private final LinkedBlockingDeque<CompletableFuture<Circuit.HopMessage>> queue = new LinkedBlockingDeque<>();
+
+        public Sender(Stream stream) {
+            this.stream = stream;
+        }
+
+        @Override
+        public void onMessage(@NotNull Stream stream, Circuit.HopMessage msg) {
+            queue.poll().complete(msg);
+        }
+
+        public CompletableFuture<Circuit.HopMessage> rpc(Circuit.HopMessage req) {
+            CompletableFuture<Circuit.HopMessage> res = new CompletableFuture<>();
+            queue.add(res);
+            stream.writeAndFlush(req);
+            return res;
+        }
+
+        @Override
+        public CompletableFuture<Stream> connect(PeerId target) {
+            return rpc(Circuit.HopMessage.newBuilder()
+                    .setType(Circuit.HopMessage.Type.CONNECT)
+                    .setPeer(Circuit.Peer.newBuilder().setId(ByteString.copyFrom(target.getBytes())))
+                    .build()).thenApply(msg -> {
+                        if (msg.getType() == Circuit.HopMessage.Type.STATUS && msg.getStatus() == Circuit.Status.OK)
+                            return stream;
+                        throw new IllegalStateException("Circuit dial returned " + msg.getStatus().name());
+            });
+        }
+    }
+
+    public static class Receiver implements ProtocolMessageHandler<Circuit.HopMessage>, HopController {
+        private final Host us;
+        private final RelayManager manager;
+        private final Supplier<List<Multiaddr>> publicAddresses;
+        private final CircuitStopProtocol.Binding stop;
+        private final AddressBook addressBook;
+
+        public Receiver(Host us,
+                        RelayManager manager,
+                        Supplier<List<Multiaddr>> publicAddresses,
+                        CircuitStopProtocol.Binding stop,
+                        AddressBook addressBook) {
+            this.us = us;
+            this.manager = manager;
+            this.publicAddresses = publicAddresses;
+            this.stop = stop;
+            this.addressBook = addressBook;
+        }
+
+        @Override
+        public void onMessage(@NotNull Stream stream, Circuit.HopMessage msg) {
+            switch (msg.getType()) {
+                case RESERVE: {
+                    PeerId requestor = stream.remotePeerId();
+                    Optional<Reservation> reservation = manager.createReservation(requestor);
+                    if (reservation.isEmpty() || new Multiaddr(stream.getConnection().remoteAddress().toString()).has(Protocol.P2PCIRCUIT)) {
+                        stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                                .setType(Circuit.HopMessage.Type.STATUS)
+                                .setStatus(Circuit.Status.RESERVATION_REFUSED));
+                        return;
+                    }
+                    Reservation resv = reservation.get();
+                    stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                            .setType(Circuit.HopMessage.Type.STATUS)
+                            .setStatus(Circuit.Status.OK)
+                            .setReservation(Circuit.Reservation.newBuilder()
+                                    .setExpire(resv.expiry.toEpochSecond(ZoneOffset.UTC))
+                                    .addAllAddrs(publicAddresses.get().stream()
+                                            .map(a -> ByteString.copyFrom(a.serialize()))
+                                            .collect(Collectors.toList()))
+                                    .setVoucher(ByteString.copyFrom(resv.voucher)))
+                            .setLimit(Circuit.Limit.newBuilder()
+                                    .setDuration(resv.durationSeconds)
+                                    .setData(resv.maxBytes)));
+                } case CONNECT: {
+                    PeerId target = new PeerId(msg.getPeer().getId().toByteArray());
+                    if (manager.hasReservation(target)) {
+                        PeerId initiator = stream.remotePeerId();
+                        Optional<Reservation> res = manager.allowConnection(target, initiator);
+                        if (res.isPresent()) {
+                            Reservation resv = res.get();
+                            try {
+                                CircuitStopProtocol.StopController stop = this.stop.dial(us, target,
+                                                addressBook.getAddrs(target).join().toArray(new Multiaddr[0])).getController()
+                                        .orTimeout(15, TimeUnit.SECONDS).join();
+                                Circuit.StopMessage reply = stop.connect(initiator, resv.durationSeconds, resv.maxBytes).join();
+                                if (reply.getStatus().equals(Circuit.Status.OK)) {
+                                    stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                                            .setType(Circuit.HopMessage.Type.STATUS)
+                                            .setStatus(Circuit.Status.OK));
+                                    Stream toTarget = stop.getStream();
+                                    Stream fromRequestor = stream;
+                                    // connect these streams with time + bytes enforcement
+                                    fromRequestor.pushHandler(new InboundTrafficLimitHandler(resv.maxBytes));
+                                    fromRequestor.pushHandler(new TotalTimeoutHandler(Duration.of(resv.durationSeconds, ChronoUnit.SECONDS)));
+                                    toTarget.pushHandler(new InboundTrafficLimitHandler(resv.maxBytes));
+                                    toTarget.pushHandler(new TotalTimeoutHandler(Duration.of(resv.durationSeconds, ChronoUnit.SECONDS)));
+                                    fromRequestor.pushHandler(new ProxyHandler(toTarget));
+                                    toTarget.pushHandler(new ProxyHandler(fromRequestor));
+                                } else {
+                                    stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                                            .setType(Circuit.HopMessage.Type.STATUS)
+                                            .setStatus(reply.getStatus()));
+                                }
+                            } catch (Exception e) {
+                                stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                                        .setType(Circuit.HopMessage.Type.STATUS)
+                                        .setStatus(Circuit.Status.CONNECTION_FAILED));
+                            }
+                        } else {
+                            stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                                    .setType(Circuit.HopMessage.Type.STATUS)
+                                    .setStatus(Circuit.Status.RESOURCE_LIMIT_EXCEEDED));
+                        }
+                    } else {
+                        stream.writeAndFlush(Circuit.HopMessage.newBuilder()
+                                .setType(Circuit.HopMessage.Type.STATUS)
+                                .setStatus(Circuit.Status.NO_RESERVATION));
+                    }
+                }
+            }
+        }
+
+        @Override
+        public CompletableFuture<Stream> connect(PeerId target) {
+            return CompletableFuture.failedFuture(new IllegalStateException("Cannot send from a receiver!"));
+        }
+
+        public CompletableFuture<Circuit.HopMessage> rpc(Circuit.HopMessage msg) {
+            return CompletableFuture.failedFuture(new IllegalStateException("Cannot send from a receiver!"));
+        }
+    }
+
+    private static class ProxyHandler extends ChannelInboundHandlerAdapter {
+
+        private final Stream target;
+
+        public ProxyHandler(Stream target) {
+            this.target = target;
+        }
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            if (msg instanceof ByteBuf) {
+                target.writeAndFlush(msg);
+                ((ByteBuf) msg).release();
+            }
+        }
+    }
+
+    private static final int TRAFFIC_LIMIT = 2*1024;
+    private final RelayManager manager;
+    private final CircuitStopProtocol.Binding stop;
+    private Host us;
+
+    public CircuitHopProtocol(RelayManager manager,
+                              CircuitStopProtocol.Binding stop) {
+        super(Circuit.HopMessage.getDefaultInstance(), TRAFFIC_LIMIT, TRAFFIC_LIMIT);
+        this.manager = manager;
+        this.stop = stop;
+    }
+
+    public void setHost(Host us) {
+        this.us = us;
+    }
+
+    @NotNull
+    @Override
+    protected CompletableFuture<HopController> onStartInitiator(@NotNull Stream stream) {
+        Sender replyPropagator = new Sender(stream);
+        stream.pushHandler(replyPropagator);
+        return CompletableFuture.completedFuture(replyPropagator);
+    }
+
+    @NotNull
+    @Override
+    protected CompletableFuture<HopController> onStartResponder(@NotNull Stream stream) {
+        if (us == null)
+            throw new IllegalStateException("null Host for us!");
+        Supplier<List<Multiaddr>> ourpublicAddresses = () -> us.getAddressBook().get(us.getPeerId()).join()
+                .stream()
+                .map(a -> new Multiaddr(a.toString()))
+                .collect(Collectors.toList());
+        Receiver dialer = new Receiver(us, manager, ourpublicAddresses, stop, us.getAddressBook());
+        stream.pushHandler(dialer);
+        return CompletableFuture.completedFuture(dialer);
+    }
+}
---
+++ libp2p/src/main/java/io/libp2p/protocol/circuit/RelayTransport.java
@@ -0,0 +1,304 @@
+package io.libp2p.protocol.circuit;
+
+import io.libp2p.core.*;
+import io.libp2p.core.Stream;
+import io.libp2p.core.multiformats.*;
+import io.libp2p.core.mux.*;
+import io.libp2p.core.security.*;
+import io.libp2p.core.transport.*;
+import io.libp2p.etc.*;
+import io.libp2p.transport.*;
+import io.netty.channel.*;
+import kotlin.*;
+import org.jetbrains.annotations.*;
+
+import java.time.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
+import java.util.function.Function;
+import java.util.stream.*;
+
+public class RelayTransport implements Transport, HostConsumer {
+    private Host us;
+    private final Map<PeerId, RelayState> listeners = new ConcurrentHashMap<>();
+    private final Map<Multiaddr, Stream> dials = new ConcurrentHashMap<>();
+    private final Function<Host, List<CandidateRelay>> candidateRelays;
+    private final CircuitHopProtocol.Binding hop;
+    private final CircuitStopProtocol.Binding stop;
+    public final ConnectionUpgrader upgrader;
+    private final AtomicInteger relayCount;
+
+    public RelayTransport(CircuitHopProtocol.Binding hop,
+                          CircuitStopProtocol.Binding stop,
+                          ConnectionUpgrader upgrader,
+                          Function<Host, List<CandidateRelay>> candidateRelays) {
+        this.hop = hop;
+        this.stop = stop;
+        this.upgrader = upgrader;
+        this.candidateRelays = candidateRelays;
+        this.relayCount = new AtomicInteger(0);
+    }
+
+    @Override
+    public void setHost(Host us) {
+        this.us = us;
+    }
+
+    public static class CandidateRelay {
+        public final PeerId id;
+        public final List<Multiaddr> addrs;
+
+        public CandidateRelay(PeerId id, List<Multiaddr> addrs) {
+            this.id = id;
+            this.addrs = addrs;
+        }
+    }
+
+    private static class RelayState {
+        List<Multiaddr> addrs;
+        CircuitHopProtocol.HopController controller;
+        Connection conn;
+        LocalDateTime renewAfter;
+    }
+
+    public void setRelayCount(int count) {
+        relayCount.set(count);
+    }
+
+    @Override
+    public int getActiveConnections() {
+        return dials.size();
+    }
+
+    @Override
+    public int getActiveListeners() {
+        return listeners.size();
+    }
+
+    @NotNull
+    @Override
+    public CompletableFuture<Unit> close() {
+        return CompletableFuture.allOf(dials.values()
+                        .stream()
+                        .map(Stream::close)
+                        .toArray(CompletableFuture[]::new))
+                .thenApply(x -> {
+                    dials.clear();
+                    return null;
+                });
+    }
+
+    static class ConnectionOverStream implements Connection {
+        private final boolean isInitiator;
+        private final Transport transport;
+        private final Stream stream;
+        private SecureChannel.Session security;
+        private StreamMuxer.Session muxer;
+
+        public ConnectionOverStream(boolean isInitiator, Transport transport, Stream stream) {
+            this.isInitiator = isInitiator;
+            this.transport = transport;
+            this.stream = stream;
+        }
+
+        @NotNull
+        @Override
+        public Multiaddr localAddress() {
+            return stream.getConnection().localAddress().withComponent(Protocol.P2PCIRCUIT);
+        }
+
+        @NotNull
+        @Override
+        public Multiaddr remoteAddress() {
+            return stream.getConnection().remoteAddress().withComponent(Protocol.P2PCIRCUIT);
+        }
+
+        public void setSecureSession(SecureChannel.Session sec) {
+            this.security = sec;
+        }
+
+        @NotNull
+        @Override
+        public SecureChannel.Session secureSession() {
+            return security;
+        }
+
+        public void setMuxerSession(StreamMuxer.Session mux) {
+            this.muxer = mux;
+        }
+
+        @NotNull
+        @Override
+        public StreamMuxer.Session muxerSession() {
+            return muxer;
+        }
+
+        @NotNull
+        @Override
+        public Transport transport() {
+            return transport;
+        }
+
+        @Override
+        public boolean isInitiator() {
+            return isInitiator;
+        }
+
+        @Override
+        public void addHandlerBefore(@NotNull String s, @NotNull String s1, @NotNull ChannelHandler channelHandler) {
+            stream.addHandlerBefore(s, s1, channelHandler);
+        }
+
+        @NotNull
+        @Override
+        public CompletableFuture<Unit> close() {
+            return stream.close();
+        }
+
+        @NotNull
+        @Override
+        public CompletableFuture<Unit> closeFuture() {
+            return stream.closeFuture();
+        }
+
+        @Override
+        public void pushHandler(@NotNull ChannelHandler channelHandler) {
+            stream.pushHandler(channelHandler);
+        }
+
+        @Override
+        public void pushHandler(@NotNull String s, @NotNull ChannelHandler channelHandler) {
+            stream.pushHandler(s, channelHandler);
+        }
+    }
+
+    @NotNull
+    @Override
+    public CompletableFuture<Connection> dial(@NotNull Multiaddr multiaddr,
+                                              @NotNull ConnectionHandler connHandler,
+                                              @Nullable ChannelVisitor<P2PChannel> channelVisitor) {
+        // first connect to relay over hop
+        List<MultiaddrComponent> comps = multiaddr.getComponents();
+        int split = comps.indexOf(new MultiaddrComponent(Protocol.P2PCIRCUIT, null));
+        Multiaddr relay = new Multiaddr(comps.subList(0, split));
+        Multiaddr target = new Multiaddr(comps.subList(split, comps.size()));
+        CircuitHopProtocol.HopController ctr = hop.dial(us, relay).getController().join();
+        // request proxy to target
+        Stream stream = ctr.connect(target.getPeerId()).join();
+        // upgrade with sec and muxer
+        return upgradeStream(stream, true, upgrader, this, target.getPeerId(), connHandler);
+    }
+
+    public static CompletableFuture<Connection> upgradeStream(Stream stream,
+                                                              boolean isInitiator,
+                                                              ConnectionUpgrader upgrader,
+                                                              Transport transport,
+                                                              PeerId remote,
+                                                              ConnectionHandler connHandler) {
+        ConnectionOverStream conn = new ConnectionOverStream(isInitiator, transport, stream);
+        CompletableFuture<Connection> res = new CompletableFuture<>();
+        stream.pushHandler(new ChannelInitializer<>() {
+            @Override
+            protected void initChannel(Channel channel) throws Exception {
+                System.out.println("Upgrade outgoing relay to " + remote);
+                channel.attr(AttributesKt.getREMOTE_PEER_ID()).set(remote);
+                upgrader.establishSecureChannel(conn)
+                        .thenCompose(sess -> {
+                            conn.setSecureSession(sess);
+                            if (sess.getEarlyMuxer() != null) {
+                                return ConnectionUpgrader.Companion.establishMuxer(sess.getEarlyMuxer(), conn);
+                            } else {
+                                return upgrader.establishMuxer(conn);
+                            }
+                        }).thenAccept(sess -> {
+                            conn.setMuxerSession(sess);
+                            connHandler.handleConnection(conn);
+                            res.complete(conn);
+                        }).exceptionally(t -> {
+                            res.completeExceptionally(t);
+                            return null;
+                        });
+            }
+        });
+        return res;
+    }
+
+    @Override
+    public boolean handles(@NotNull Multiaddr multiaddr) {
+        return multiaddr.hasAny(Protocol.P2PCIRCUIT);
+    }
+
+    @Override
+    public void initialize() {
+        stop.setTransport(this);
+        // find relays and connect and reserve
+        new Thread(() -> {
+            while (true) {
+                ensureEnoughCurrentRelays();
+                try {Thread.sleep(2*60_000);} catch (InterruptedException e) {}
+            }
+        }, "RelayManager").start();
+    }
+
+    public void ensureEnoughCurrentRelays() {
+        int active = 0;
+        // renew existing relays before finding new ones
+        Set<Map.Entry<PeerId, RelayState>> currentRelays = listeners.entrySet();
+        for (Map.Entry<PeerId, RelayState> current : currentRelays) {
+            RelayState relay = current.getValue();
+            LocalDateTime now = LocalDateTime.now();
+            if (now.isBefore(relay.renewAfter)) {
+                active++;
+            } else {
+                try {
+                    CircuitHopProtocol.Reservation reservation = relay.controller.reserve().join();
+                    relay.renewAfter = reservation.expiry.minusMinutes(1);
+                    active++;
+                } catch (Exception e) {
+                    listeners.remove(current.getKey());
+                }
+            }
+        }
+        if (active >= relayCount.get())
+            return;
+
+        List<CandidateRelay> candidates = candidateRelays.apply(us);
+        for (CandidateRelay candidate : candidates) {
+            // connect to relay and get reservation
+            CircuitHopProtocol.HopController ctr = hop.dial(us, candidate.id, candidate.addrs.toArray(new Multiaddr[0]))
+                    .getController().join();
+            CircuitHopProtocol.Reservation resv = ctr.reserve().join();
+            active++;
+            listeners.put(candidate.id, new RelayState());
+            if (active >= relayCount.get())
+                return;
+        }
+    }
+
+    @NotNull
+    @Override
+    public CompletableFuture<Unit> listen(@NotNull Multiaddr multiaddr, @NotNull ConnectionHandler connectionHandler, @Nullable ChannelVisitor<P2PChannel> channelVisitor) {
+        throw new IllegalStateException("Invalid operation on RelayTransport!");
+    }
+
+    @NotNull
+    @Override
+    public List<Multiaddr> listenAddresses() {
+        return listeners.entrySet().stream()
+                .flatMap(r -> r.getValue().addrs.stream()
+                        .map(a -> a.withP2P(r.getKey())
+                                .concatenated(new Multiaddr(List.of(new MultiaddrComponent(Protocol.P2PCIRCUIT, null)))
+                                        .withP2P(us.getPeerId()))))
+                .collect(Collectors.toList());
+    }
+
+    @NotNull
+    @Override
+    public CompletableFuture<Unit> unlisten(@NotNull Multiaddr multiaddr) {
+        RelayState relayState = listeners.get(multiaddr);
+        if (relayState == null)
+            return CompletableFuture.completedFuture(null);
+        return relayState.conn.close();
+    }
+}
