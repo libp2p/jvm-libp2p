02fec6958aba4fdb455712d10338c7643af8abe6
ian <ianopolous@protonmail.com>
2024-01-04 23:41:12 +1100

    Add unit test for relay bandwidth limit

--- libp2p/src/test/java/io/libp2p/core/RelayTestJava.java
+++ libp2p/src/test/java/io/libp2p/core/RelayTestJava.java
@@ -127,4 +127,99 @@ public class RelayTestJava {
     serverHost.stop().get(5, TimeUnit.SECONDS);
     System.out.println("Server stopped");
   }
+
+  @Test
+  void relayStreamsAreLimited() throws Exception {
+    String localListenAddress = "/ip4/127.0.0.1/tcp/40002";
+
+    Host relayHost =
+        new HostBuilder()
+            .builderModifier(b -> enableRelay(b, Collections.emptyList()))
+            .transport(TcpTransport::new)
+            .secureChannel(NoiseXXSecureChannel::new)
+            .muxer(StreamMuxerProtocol::getYamux)
+            .listen("/ip4/127.0.0.1/tcp/0")
+            .build();
+    relayHost.getNetwork().getTransports().stream()
+        .filter(t -> t instanceof RelayTransport)
+        .map(t -> (RelayTransport) t)
+        .findFirst()
+        .get()
+        .setHost(relayHost);
+    CompletableFuture<Void> relayStarted = relayHost.start();
+    relayStarted.get(5, TimeUnit.SECONDS);
+
+    List<Multiaddr> relayAddrs = relayHost.listenAddresses();
+    Multiaddr relayAddr = relayAddrs.get(0);
+    RelayTransport.CandidateRelay relay =
+        new RelayTransport.CandidateRelay(relayHost.getPeerId(), relayAddrs);
+    List<RelayTransport.CandidateRelay> relays = List.of(relay);
+
+    // Relay streams are limited to 4096 bytes in either direction
+    // This is the smallest value that triggers the limit
+    // not sure why there is so much overhead from 3 * multistream + noise + yamux!
+    int blobSize = 1469;
+    Host clientHost =
+        new HostBuilder()
+            .builderModifier(b -> enableRelay(b, relays))
+            .transport(TcpTransport::new)
+            .secureChannel(NoiseXXSecureChannel::new)
+            .muxer(StreamMuxerProtocol::getYamux)
+            .protocol(new Blob(blobSize))
+            .build();
+    clientHost.getNetwork().getTransports().stream()
+        .filter(t -> t instanceof RelayTransport)
+        .map(t -> (RelayTransport) t)
+        .findFirst()
+        .get()
+        .setHost(clientHost);
+
+    Host serverHost =
+        new HostBuilder()
+            .builderModifier(b -> enableRelay(b, relays))
+            .transport(TcpTransport::new)
+            .secureChannel(NoiseXXSecureChannel::new)
+            .muxer(StreamMuxerProtocol::getYamux)
+            .protocol(new Blob(blobSize))
+            .listen(localListenAddress)
+            .listen(relayAddr + "/p2p-circuit")
+            .build();
+    serverHost.getNetwork().getTransports().stream()
+        .filter(t -> t instanceof RelayTransport)
+        .map(t -> (RelayTransport) t)
+        .findFirst()
+        .get()
+        .setHost(serverHost);
+
+    CompletableFuture<Void> clientStarted = clientHost.start();
+    CompletableFuture<Void> serverStarted = serverHost.start();
+    clientStarted.get(5, TimeUnit.SECONDS);
+    System.out.println("Client started");
+    serverStarted.get(5, TimeUnit.SECONDS);
+    System.out.println("Server started");
+
+    Multiaddr toDial =
+        relayAddr.concatenated(
+            new Multiaddr("/p2p-circuit/p2p/" + serverHost.getPeerId().toBase58()));
+    System.out.println("Dialling " + toDial + " from " + clientHost.getPeerId());
+    StreamPromise<BlobController> blob =
+        clientHost
+            .getNetwork()
+            .connect(serverHost.getPeerId(), toDial)
+            .thenApply(it -> it.muxerSession().createStream(new Blob(blobSize)))
+            .get(5, TimeUnit.SECONDS);
+
+    Stream blobStream = blob.getStream().get(5, TimeUnit.SECONDS);
+    System.out.println("Blob stream created");
+    BlobController blobCtr = blob.getController().get(5, TimeUnit.SECONDS);
+    System.out.println("Blob controller created");
+
+    Assertions.assertThrows(
+        ExecutionException.class, () -> blobCtr.blob().get(5, TimeUnit.SECONDS));
+
+    clientHost.stop().get(5, TimeUnit.SECONDS);
+    System.out.println("Client stopped");
+    serverHost.stop().get(5, TimeUnit.SECONDS);
+    System.out.println("Server stopped");
+  }
 }
