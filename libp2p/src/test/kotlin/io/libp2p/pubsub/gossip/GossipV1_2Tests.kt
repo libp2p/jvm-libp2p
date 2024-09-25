a9f8bb6f03b9f43c23a6a90090c73a007bbf7635
Stefan Bratanov <stefan.bratanov93@gmail.com>
2024-09-25 15:30:22 +0100

    add tests

--- libp2p/src/test/kotlin/io/libp2p/pubsub/gossip/GossipV1_2Tests.kt
+++ libp2p/src/test/kotlin/io/libp2p/pubsub/gossip/GossipV1_2Tests.kt
@@ -4,6 +4,7 @@ package io.libp2p.pubsub.gossip
 
 import io.libp2p.etc.types.millis
 import io.libp2p.etc.types.seconds
+import io.libp2p.etc.types.toProtobuf
 import io.libp2p.etc.types.toWBytes
 import io.libp2p.pubsub.PubsubProtocol
 import org.assertj.core.api.Assertions.assertThat
@@ -54,11 +55,56 @@ class GossipV1_2Tests : GossipTestsBase() {
             .filter { it.hasControl() }
             .flatMap { it.control.idontwantList }
 
-        // both mock peers should have received IDONTWANT from the GossipRouter
+        // both gossipers should have received IDONTWANT from the GossipRouter
         assertTrue(iDontWants.size == 2)
 
         iDontWants.forEach { iDontWant ->
             assertThat(iDontWant.messageIDsList.map { it.toWBytes() }).containsExactly(msg.messageId)
         }
+    }
+
+    @Test
+    fun messageIsNotBroadcastIfPeerHasSentIDONTWANT() {
+        val test = ManyRoutersTest(
+            protocol = PubsubProtocol.Gossip_V_1_2,
+            params = GossipParams(iDontWantMinMessageSizeThreshold = 5),
+            mockRouterCount = 2
+        )
+
+        test.connectAll()
+
+        test.gossipRouter.subscribe("topic1")
+        test.mockRouters.forEach { it.subscribe("topic1") }
+
+        // 2 heartbeats - the topic should be GRAFTed
+        test.fuzz.timeController.addTime(2.seconds)
+
+        val publisher = test.mockRouters[0]
+        val iDontWantPeer = test.mockRouters[1]
+
+        val msg = newMessage("topic1", 0L, "Hello".toByteArray())
+
+        // sending IDONTWANT
+        iDontWantPeer.sendToSingle(
+            Rpc.RPC.newBuilder().setControl(
+                Rpc.ControlMessage.newBuilder().addIdontwant(
+                    Rpc.ControlIDontWant.newBuilder()
+                        .addMessageIDs(msg.messageId.toProtobuf())
+                )
+            ).build()
+        )
+
+        test.fuzz.timeController.addTime(100.millis)
+
+        publisher.sendToSingle(
+            Rpc.RPC.newBuilder().addPublish(msg.protobufMessage).build()
+        )
+
+        test.fuzz.timeController.addTime(100.millis)
+
+        val receivedMessages = iDontWantPeer.inboundMessages.flatMap { it.publishList }
+
+        // message shouldn't have been received
+        assertThat(receivedMessages).isEmpty()
     }
 }
