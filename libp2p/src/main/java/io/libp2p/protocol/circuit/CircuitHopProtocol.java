f0e2ef41b72fb738d596bb63aad1d28d32e72e0a
Ryu Seowoong <singlerr@naver.com>
2025-08-28 22:56:56 +0900

    fix: executed spotless apply

--- libp2p/src/main/java/io/libp2p/protocol/circuit/CircuitHopProtocol.java
+++ libp2p/src/main/java/io/libp2p/protocol/circuit/CircuitHopProtocol.java
@@ -315,13 +315,13 @@ public class CircuitHopProtocol extends ProtobufProtocolHandler<CircuitHopProtoc
                         new CircuitStopProtocol.StopRemover());
 
                     // connect these streams with time + bytes enforcement
-                    if(resv.maxBytes > 0){
+                    if (resv.maxBytes > 0) {
                       fromRequestor.pushHandler(new InboundTrafficLimitHandler(resv.maxBytes));
                     }
                     fromRequestor.pushHandler(
                         new TotalTimeoutHandler(
                             Duration.of(resv.durationSeconds, ChronoUnit.SECONDS)));
-                    if(resv.maxBytes > 0){
+                    if (resv.maxBytes > 0) {
                       toTarget.pushHandler(new InboundTrafficLimitHandler(resv.maxBytes));
                     }
                     toTarget.pushHandler(
